fd54658e53c06e3f513f25e4cd8ab616e2144158
linyuchen <lin.yu.chen@hotmail.com>
2024-04-15 22:42:31 +0800

    todo: retry get groups if groups length is 0

--- src/onebot11/action/group/GetGroupList.ts
+++ src/onebot11/action/group/GetGroupList.ts
@@ -3,16 +3,18 @@ import { OB11Constructor } from '../../constructor';
 import BaseAction from '../BaseAction';
 import { ActionName } from '../types';
 import { groups } from '@/common/data';
+import {NTQQGroupApi} from "@/core/qqnt/apis";
 
 
 class GetGroupList extends BaseAction<null, OB11Group[]> {
   actionName = ActionName.GetGroupList;
 
   protected async _handle(payload: null) {
-    // if (groups.length === 0) {
-    //     const groups = await NTQQGroupApi.getGroups(true)
-    //     log("get groups", groups)
-    // }
+    if (groups.size === 0) {
+      //todo: get groups
+        // const groups = await NTQQGroupApi.getGroups(true)
+        // log("get groups", groups)
+    }
     return OB11Constructor.groups(Array.from(groups.values()));
   }
 }
