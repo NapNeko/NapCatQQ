e516687a9e71da3b62f34a978feec8f36da28d3e
手瓜一十雪 <nanaeonn@outlook.com>
2024-04-16 13:50:01 +0800

    fix

--- script/checkVersion.cjs
+++ script/checkVersion.cjs
@@ -12,5 +12,5 @@ if (currentVersion === targetVersion) {
     packageJson.version = targetVersion;
     fs.writeFileSync("./package.json", JSON.stringify(packageJson));
     let runscript = "sed -i 's/\"version\": \"" + currentVersion + "\"/\"version\": \"" + targetVersion + "\"/g' package.json";
-    fs.writeFileSync("./checkVersion.sh", "#!/bin/bashe\ngit config --global user.email \"bot@test.mail\"\n git config --global user.name \"Version\"\n" + runscript + "\ngit add .\n git commit -m \"chore:version change\"\n git push -u origin main")
+    fs.writeFileSync("./checkVersion.sh", "#!/bin/bashe\ngit config --global user.email \"bot@test.nanaeo.cn\"\n git config --global user.name \"Version\"\n" + runscript + "\ngit add .\n git commit -m \"chore:version change\"\n git push -u origin main")
 }
