a8bce36f3b1e9dfd338717b61ae8077650edb895
linyuchen <lin.yu.chen@hotmail.com>
2024-04-27 21:06:58 +0800

    fix: first get image rkey

--- src/common/utils/system.ts
+++ src/common/utils/system.ts
@@ -2,6 +2,7 @@ import os from 'node:os';
 import path from 'node:path';
 
 export const systemPlatform = os.platform();
+export const cpuArch = os.arch();
 export const systemVersion = os.release();
 export const hostname = os.hostname();
 const homeDir = os.homedir();
