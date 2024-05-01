8d1b4f61e7d76a585108888504602849122f46b3
linyuchen <lin.yu.chen@hotmail.com>
2024-05-01 18:35:16 +0800

    fix: listener crash

--- src/onebot11/action/group/GetGroupMemberInfo.ts
+++ src/onebot11/action/group/GetGroupMemberInfo.ts
@@ -1,5 +1,5 @@
 import { OB11GroupMember } from '../../types';
-import { getGroupMember } from '@/core/data';
+import { getGroup, getGroupMember } from '@/core/data';
 import { OB11Constructor } from '../../constructor';
 import BaseAction from '../BaseAction';
 import { ActionName } from '../types';
@@ -18,6 +18,10 @@ class GetGroupMemberInfo extends BaseAction<PayloadType, OB11GroupMember> {
   actionName = ActionName.GetGroupMemberInfo;
 
   protected async _handle(payload: PayloadType) {
+    const group = await getGroup(payload.group_id.toString());
+    if (!group) {
+      throw (`群(${payload.group_id})不存在`);
+    }
     const WebGroupMember = await WebApi.getGroupMembers(payload.group_id.toString());
     const member = await getGroupMember(payload.group_id.toString(), payload.user_id.toString());
     // log(member);
