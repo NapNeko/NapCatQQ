a44256fd1cd358aae7e4dfee892e119702c97ead
linyuchen <lin.yu.chen@hotmail.com>
2024-05-04 21:56:45 +0800

    remove webapi get group members

--- src/onebot11/action/group/GetGroupMemberInfo.ts
+++ src/onebot11/action/group/GetGroupMemberInfo.ts
@@ -6,12 +6,13 @@ import { ActionName } from '../types';
 import { NTQQUserApi } from '@/core/apis/user';
 import { log, logDebug } from '@/common/utils/log';
 import { isNull } from '../../../common/utils/helper';
-import { WebApi } from '@/core/apis/webapi';
+import { WebApi, WebApiGroupMember } from '@/core/apis/webapi';
 
 
 export interface PayloadType {
   group_id: number;
   user_id: number;
+  no_cache?: boolean | string;
 }
 
 class GetGroupMemberInfo extends BaseAction<PayloadType, OB11GroupMember> {
@@ -22,7 +23,10 @@ class GetGroupMemberInfo extends BaseAction<PayloadType, OB11GroupMember> {
     if (!group) {
       throw (`群(${payload.group_id})不存在`);
     }
-    const WebGroupMember = await WebApi.getGroupMembers(payload.group_id.toString());
+    const webGroupMembers: WebApiGroupMember[] = [];
+    if (payload.no_cache === true || payload.no_cache === 'true') {
+      // webGroupMembers = await WebApi.getGroupMembers(payload.group_id.toString());
+    }
     const member = await getGroupMember(payload.group_id.toString(), payload.user_id.toString());
     // log(member);
     if (member) {
@@ -35,14 +39,13 @@ class GetGroupMemberInfo extends BaseAction<PayloadType, OB11GroupMember> {
         logDebug('获取群成员详细信息失败, 只能返回基础信息', e);
       }
       const retMember = OB11Constructor.groupMember(payload.group_id.toString(), member);
-      for (let i = 0, len = WebGroupMember.length; i < len; i++) {
-        if (WebGroupMember[i]?.uin && WebGroupMember[i].uin === retMember.user_id) {
-          retMember.join_time = WebGroupMember[i]?.join_time;
-          retMember.last_sent_time = WebGroupMember[i]?.last_speak_time;
-          retMember.qage = WebGroupMember[i]?.qage;
-          retMember.level = WebGroupMember[i]?.lv.level;
+      for (let i = 0, len = webGroupMembers.length; i < len; i++) {
+        if (webGroupMembers[i]?.uin && webGroupMembers[i].uin === retMember.user_id) {
+          retMember.join_time = webGroupMembers[i]?.join_time;
+          retMember.last_sent_time = webGroupMembers[i]?.last_speak_time;
+          retMember.qage = webGroupMembers[i]?.qage;
+          retMember.level = webGroupMembers[i]?.lv.level;
         }
-
       }
       return retMember;
     } else {
--- src/onebot11/server/ws/ReverseWebsocket.ts
+++ src/onebot11/server/ws/ReverseWebsocket.ts
@@ -101,7 +101,9 @@ export class ReverseWebsocket {
       await this.onmessage(data.toString());
     });
 
-    this.websocket.on('error', log);
+    this.websocket.on('error', (err) => {
+      logError('websocket error', err.toString());
+    });
 
     const wsClientInterval = setInterval(() => {
       postWsEvent(new OB11HeartbeatEvent(!!selfInfo.online, true, heartInterval));
