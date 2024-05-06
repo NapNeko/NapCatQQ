06d2aa0373ef4c83a8b3e9b1791fc0abcbf3f245
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-06 23:02:00 +0800

    feat: webui LoginStatus

--- src/webui/src/api/LogConsole.ts
+++ src/webui/src/api/LogConsole.ts
@@ -1,5 +1,6 @@
 import { RequestHandler } from "express";
+import { resolve } from "path";
 
 export const GetLogHandler: RequestHandler = (req, res) => {
-
+    //res.sendFile(resolve(__dirname, "../../../logs/napcat.log"));
 };
