05051cf0cda7c102319b77290f892056129b4a10
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-08 14:29:18 +0800

    fix: webui Confi&webui Login

--- src/webui/src/helper/config.ts
+++ src/webui/src/helper/config.ts
@@ -41,34 +41,40 @@ export interface WebUiConfigType {
     token: string;
     loginRate: number
 }
-async function GetWebUIConfig(): Promise<WebUiConfigType> {
-    try {
-        let configPath = resolve(__dirname, "./config/webui.json");
-        let config: WebUiConfigType = {
-            port: 6099,
-            token: Math.random().toString(36).slice(2),//生成随机密码
-            loginRate: 3
-        };
-
-        if (!existsSync(configPath)) {
-            writeFileSync(configPath, JSON.stringify(config, null, 4));
-        }
-
-        let fileContent = readFileSync(configPath, "utf-8");
-        let parsedConfig = JSON.parse(fileContent) as WebUiConfigType;
-
-        // 修正端口占用情况
-        const [err, data] = await tryUsePort(parsedConfig.port).then(data => [null, data as number]).catch(err => [err, null]);
-        parsedConfig.port = data;
-        if (err) {
-            //一般没那么离谱 如果真有这么离谱 考虑下 向外抛出异常
-        }
-        return parsedConfig;
-    } catch (e) {
-        console.error("读取配置文件失败", e);
-    }
-    return {} as WebUiConfigType; // 理论上这行代码到不了，为了保持函数完整性而保留
-}
-
 // 读取当前目录下名为 webui.json 的配置文件，如果不存在则创建初始化配置文件
-export const WebUiConfig = await GetWebUIConfig();
+class WebUiConfigWrapper {
+    WebUiConfigData: WebUiConfigType | undefined = undefined;
+    async GetWebUIConfig(): Promise<WebUiConfigType> {
+        if (this.WebUiConfigData) {
+            return this.WebUiConfigData;
+        }
+        try {
+            let configPath = resolve(__dirname, "./config/webui.json");
+            let config: WebUiConfigType = {
+                port: 6099,
+                token: Math.random().toString(36).slice(2),//生成随机密码
+                loginRate: 3
+            };
+
+            if (!existsSync(configPath)) {
+                writeFileSync(configPath, JSON.stringify(config, null, 4));
+            }
+
+            let fileContent = readFileSync(configPath, "utf-8");
+            let parsedConfig = JSON.parse(fileContent) as WebUiConfigType;
+
+            // 修正端口占用情况
+            const [err, data] = await tryUsePort(parsedConfig.port).then(data => [null, data as number]).catch(err => [err, null]);
+            parsedConfig.port = data;
+            if (err) {
+                //一般没那么离谱 如果真有这么离谱 考虑下 向外抛出异常
+            }
+            this.WebUiConfigData = parsedConfig;
+            return this.WebUiConfigData;
+        } catch (e) {
+            console.error("读取配置文件失败", e);
+        }
+        return {} as WebUiConfigType; // 理论上这行代码到不了，为了保持函数完整性而保留
+    }
+}
+export const WebUiConfig = new WebUiConfigWrapper();
