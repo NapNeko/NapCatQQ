6f5a6f353fdbd689c9dc4afd79d7825cd2521464
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-08 15:42:22 +0800

    feat: get config

--- src/webui/src/api/OB11Config.ts
+++ src/webui/src/api/OB11Config.ts
@@ -1,12 +1,12 @@
 import { RequestHandler } from "express";
 import { DataRuntime } from "../helper/Data";
-import { existsSync } from "node:fs";
+import { existsSync, readFileSync, writeFileSync } from "node:fs";
 import { resolve } from "node:path";
 import { OB11Config } from "@/webui/ui/components/WebApi";
 const isEmpty = (data: any) => data === undefined || data === null || data === '';
 export const OB11GetConfigHandler: RequestHandler = async (req, res) => {
     let isLogin = await DataRuntime.getQQLoginStatus();
-    if(!isLogin){
+    if (!isLogin) {
         res.send({
             code: -1,
             message: 'Not Login'
@@ -15,15 +15,16 @@ export const OB11GetConfigHandler: RequestHandler = async (req, res) => {
     }
     const uin = await DataRuntime.getQQLoginUin();
     let configFilePath = resolve(__dirname, `./config/onebot_${uin}.json`);
+    console.log(configFilePath);
     let data: OB11Config;
     try {
-        data = existsSync(configFilePath) ? require(configFilePath) : require(resolve(__dirname, `./config/onebot.json`));
+        data = JSON.parse(existsSync(configFilePath) ? readFileSync(configFilePath).toString() : readFileSync(resolve(__dirname, `./config/onebot.json`)).toString());
     }
     catch (e) {
         data = {} as OB11Config;
         res.send({
             code: -1,
-            message: 'QRCode Get Error'
+            message: 'Config Get Error'
         });
         return;
     }
@@ -36,31 +37,31 @@ export const OB11GetConfigHandler: RequestHandler = async (req, res) => {
 }
 export const OB11SetConfigHandler: RequestHandler = async (req, res) => {
     let isLogin = await DataRuntime.getQQLoginStatus();
-    if(!isLogin){
+    if (!isLogin) {
         res.send({
             code: -1,
             message: 'Not Login'
         });
         return;
     }
-    if(isEmpty(req.body.config)){
+    if (isEmpty(req.body.config)) {
         res.send({
             code: -1,
             message: 'config is empty'
         });
         return;
-   }
-   let configFilePath = resolve(__dirname, `./config/onebot_${await DataRuntime.getQQLoginUin()}.json`);
-   try {
-       require(configFilePath);
-   }
-   catch (e) {
-       configFilePath = resolve(__dirname, `./config/onebot.json`);
-   }
-   require('fs').writeFileSync(configFilePath, JSON.stringify(req.body.config, null, 4));
-   res.send({
-       code: 0,
-       message: 'success'
-   });
-   return;
+    }
+    let configFilePath = resolve(__dirname, `./config/onebot_${await DataRuntime.getQQLoginUin()}.json`);
+    try {
+        require(configFilePath);
+    }
+    catch (e) {
+        configFilePath = resolve(__dirname, `./config/onebot.json`);
+    }
+    writeFileSync(configFilePath, JSON.stringify(req.body.config, null, 4));
+    res.send({
+        code: 0,
+        message: 'success'
+    });
+    return;
 }
--- src/webui/src/router/QQLogin.ts
+++ src/webui/src/router/QQLogin.ts
@@ -1,6 +1,5 @@
 import { Router } from 'express';
 import { QQCheckLoginStatusHandler, QQGetQRcodeHandler, QQGetQuickLoginListHandler } from '../api/QQLogin';
-
 const router = Router();
 router.all('/GetQuickLoginList', QQGetQuickLoginListHandler)
 router.post('/CheckLoginStatus', QQCheckLoginStatusHandler);
