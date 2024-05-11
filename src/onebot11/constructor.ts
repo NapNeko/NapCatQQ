e73096f504eb7cd5687093d4d3ca90a86a8f92f4
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-11 14:23:36 +0800

    build: 1.3.2-beta7

--- src/onebot11/constructor.ts
+++ src/onebot11/constructor.ts
@@ -92,7 +92,6 @@ export class OB11Constructor {
         resMsg.group_id = parseInt(tempGroupCode);
       }
     }
-
     for (const element of msg.elements) {
       const message_data: OB11MessageData | any = {
         data: {},
@@ -152,10 +151,11 @@ export class OB11Constructor {
         // let currentRKey = "CAQSKAB6JWENi5LMk0kc62l8Pm3Jn1dsLZHyRLAnNmHGoZ3y_gDZPqZt-64"
 
         try {
-          message_data['data']['url'] = await NTQQFileApi.getImageUrl(msg);
+          message_data['data']['url'] = await NTQQFileApi.getImageUrl(element.picElement, msg.chatType !== ChatType.group);
         } catch (e: any) {
           logError('获取图片url失败', e.stack);
         }
+        console.log(message_data['data']['url'])
         // message_data["data"]["file_id"] = element.picElement.fileUuid
         message_data['data']['file_size'] = element.picElement.fileSize;
         dbUtil.addFileCache({
