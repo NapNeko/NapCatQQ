b38f52dba934fdef57d928223a6709a229a71730
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-12 16:46:35 +0800

    chore: sync core

--- src/core.lib/src/apis/group.d.ts
+++ src/core.lib/src/apis/group.d.ts
@@ -4,6 +4,14 @@ export declare class NTQQGroupApi {
     static getGroupMembers(groupQQ: string, num?: number): Promise<Map<string, GroupMember>>;
     static getGroupNotifies(): Promise<void>;
     static getGroupIgnoreNotifies(): Promise<void>;
+    static uploadGroupBulletinPic(GroupCode: string, imageurl: string): Promise<import("@/core").GeneralCallResult & {
+        errCode: number;
+        picInfo?: {
+            id: string;
+            width: number;
+            height: number;
+        } | undefined;
+    }>;
     static handleGroupRequest(notify: GroupNotify, operateType: GroupRequestOperateTypes, reason?: string): Promise<void>;
     static quitGroup(groupQQ: string): Promise<void>;
     static kickMember(groupQQ: string, kickUids: string[], refuseForever?: boolean, kickReason?: string): Promise<void>;
@@ -16,5 +24,9 @@ export declare class NTQQGroupApi {
     static setMemberRole(groupQQ: string, memberUid: string, role: GroupMemberRole): Promise<void>;
     static setGroupName(groupQQ: string, groupName: string): Promise<void>;
     static setGroupTitle(groupQQ: string, uid: string, title: string): Promise<void>;
-    static publishGroupBulletin(groupQQ: string, title: string, content: string): void;
+    static publishGroupBulletin(groupQQ: string, content: string, picInfo?: {
+        id: string;
+        width: number;
+        height: number;
+    } | undefined, pinned?: number, confirmRequired?: number): Promise<import("@/core").GeneralCallResult>;
 }
---
+++ src/core.lib/src/utils/rkey.d.ts
@@ -0,0 +1,16 @@
+interface ServerRkeyData {
+    group_rkey: string;
+    private_rkey: string;
+    expired_time: number;
+}
+declare class RkeyManager {
+    serverUrl: string;
+    private rkeyData;
+    constructor(serverUrl: string);
+    getRkey(): Promise<ServerRkeyData>;
+    isExpired(): boolean;
+    refreshRkey(): Promise<any>;
+    fetchServerRkey(): Promise<ServerRkeyData>;
+}
+export declare const rkeyManager: RkeyManager;
+export {};
