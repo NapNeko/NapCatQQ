94273d80b0a9128ef774552ab812bc594a62cfd7
linyuchen <lin.yu.chen@hotmail.com>
2024-05-12 20:32:36 +0800

    refactor: Random os hostname

--- src/common/utils/system.ts
+++ src/common/utils/system.ts
@@ -4,9 +4,9 @@ import path from 'node:path';
 // 缓解Win7设备兼容性问题
 let osName: string;
 try {
-    osName = os.hostname();
+  osName = os.hostname();
 } catch (e) {
-    osName = "NapCat"
+  osName = 'NapCat ' + crypto.randomUUID().substring(0, 4);
 
 }
 export const systemPlatform = os.platform();
