0dacf20d78b08e02361c2b8d44ac1142a1e0c064
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-13 17:26:22 +0800

    refactor: boot script

--- script/napcat-utf8.ps1
+++ script/napcat-utf8.ps1
@@ -2,14 +2,28 @@ function Get-QQpath {
     try {
         $key = Get-ItemProperty -Path "HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\QQ"
         $uninstallString = $key.UninstallString
+	throw "get QQ path error:"
         return [System.IO.Path]::GetDirectoryName($uninstallString) + "\QQ.exe"
-    }
-    catch {
-        return "D:\QQ.exe"
+    } catch {
+        throw "get QQ path error: $_"
     }
 }
 $params = $args -join " "
-$QQpath = Get-QQpath
+Try {
+    $QQpath = Get-QQpath
+} Catch {
+        $QQpath = Read-Host -Prompt "select QQ path"
+        if (!$QQpath) {
+            Write-Host "not select QQ path, exit"
+            exit
+        }
+}
+
+if (!(Test-Path $QQpath)) {
+    throw "provided QQ path is invalid: $QQpath"
+}
+
 $Bootfile = Join-Path $PSScriptRoot "napcat.cjs"
 $env:ELECTRON_RUN_AS_NODE = 1
-Start-Process powershell -ArgumentList "-noexit", "-noprofile", "-command &{& chcp 65001;& '$QQpath' $Bootfile $params}"
+$commandInfo = Get-Command $QQpath -ErrorAction Stop
+Start-Process powershell -ArgumentList "-noexit", "-noprofile", "-command &{& chcp 65001;& '$($commandInfo.Path)' $Bootfile $params}"
