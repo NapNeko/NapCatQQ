2df61816934be06e05d4c22b76143f0e9e746cf5
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-13 17:53:50 +0800

    refactor: HttpGetJson

--- src/common/utils/request.ts
+++ src/common/utils/request.ts
@@ -18,4 +18,31 @@ export class RequestUtil {
 
     return result;
   }
+  static async HttpGetJson<T>(url: string, method: string = 'GET', data?: any, headers: Record<string, string> = {}):
+    Promise<T> {
+    let body: BodyInit | undefined = undefined;
+    let requestInit: RequestInit = { method: method };
+
+    if (method.toUpperCase() === 'POST' && data !== undefined) {
+      body = JSON.stringify(data);
+      if (headers) {
+        headers['Content-Type'] = 'application/json';
+        requestInit.headers = new Headers(headers);
+      } else {
+        requestInit.headers = new Headers({ 'Content-Type': 'application/json' });
+      }
+    } else {
+      requestInit.headers = new Headers(headers);
+    }
+    try {
+      const response = await fetch(url, { ...requestInit, body });
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      const jsonResult = await response.json();
+      return jsonResult as T;
+    } catch (error: any) {
+      throw new Error(`Failed to fetch JSON: ${error.message}`);
+    }
+  }
 }
