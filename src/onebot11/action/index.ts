750c6dfe656d90e4898e0cb891367c574395484d
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-13 09:32:25 +0800

    feat: api Extend RebootNormol

--- src/onebot11/action/index.ts
+++ src/onebot11/action/index.ts
@@ -54,9 +54,10 @@ import { GetGroupEssence } from './group/GetGroupEssence';
 import { ForwardFriendSingleMsg, ForwardGroupSingleMsg } from '@/onebot11/action/msg/ForwardSingleMsg';
 import { GetFriendWithCategory } from './extends/GetFriendWithCategory';
 import { SendGroupNotice } from './go-cqhttp/SendGroupNotice';
-import Reboot from './system/Reboot';
+import { Reboot, RebootNormol } from './system/Reboot';
 
 export const actionHandlers = [
+  new RebootNormol(),
   new GetFile(),
   new Debug(),
   new Reboot(),
