83d9c2bea15c1bc8ddd3c7c02f7b78b394cedcc2
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-13 21:07:19 +0800

    feat: 配置热重载

--- src/webui/src/helper/Data.ts
+++ src/webui/src/helper/Data.ts
@@ -1,3 +1,5 @@
+import { OB11Config } from "@/onebot11/config";
+
 interface LoginRuntimeType {
     LoginCurrentTime: number;
     LoginCurrentRate: number;
@@ -5,7 +7,8 @@ interface LoginRuntimeType {
     QQQRCodeURL: string;
     QQLoginUin: string;
     NapCatHelper: {
-        CoreQuickLogin: (uin: string) => Promise<{ result: boolean, message: string }>;
+        CoreQuickLoginCall: (uin: string) => Promise<{ result: boolean, message: string }>;
+        SetOb11ConfigCall: (ob11: OB11Config) => Promise<void>;
         QQLoginList: string[]
     }
 }
@@ -16,7 +19,8 @@ let LoginRuntime: LoginRuntimeType = {
     QQQRCodeURL: "",
     QQLoginUin: "",
     NapCatHelper: {
-        CoreQuickLogin: async (uin: string) => { return { result: false, message: '' }; },
+        SetOb11ConfigCall: async (ob11: OB11Config) => { return; },
+        CoreQuickLoginCall: async (uin: string) => { return { result: false, message: '' }; },
         QQLoginList: []
     }
 }
@@ -64,10 +68,16 @@ export const WebUiDataRuntime = {
     setQQQuickLoginList: async function (list: string[]): Promise<void> {
         LoginRuntime.NapCatHelper.QQLoginList = list;
     },
-    setQQQuickLogin(func: (uin: string) => Promise<{ result: boolean, message: string }>): void {
-        LoginRuntime.NapCatHelper.CoreQuickLogin = func;
+    setQQQuickLoginCall(func: (uin: string) => Promise<{ result: boolean, message: string }>): void {
+        LoginRuntime.NapCatHelper.CoreQuickLoginCall = func;
     },
     getQQQuickLogin: async function (uin: string): Promise<{ result: boolean, message: string }> {
-        return await LoginRuntime.NapCatHelper.CoreQuickLogin(uin);
+        return await LoginRuntime.NapCatHelper.CoreQuickLoginCall(uin);
+    },
+    setOB11ConfigCall: async function (func: (ob11: OB11Config) => Promise<void>): Promise<void> {
+        LoginRuntime.NapCatHelper.SetOb11ConfigCall = func;
+    },
+    setOB11Config: async function (ob11: OB11Config): Promise<void> {
+        await LoginRuntime.NapCatHelper.SetOb11ConfigCall(ob11);
     }
 }
