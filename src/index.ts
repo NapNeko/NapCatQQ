eb9d6240d76470df5579a8822fd2e50382f8b1f9
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-16 11:57:02 +0800

    chore: move

--- src/index.ts
+++ src/index.ts
@@ -44,7 +44,6 @@ napCatCore.onLoginSuccess((uin, uid) => {
   console.log('登录成功!');
   WebUiDataRuntime.setQQLoginStatus(true);
   WebUiDataRuntime.setQQLoginUin(uin.toString());
-  postLoginStatus().catch(logDebug);
 });
 const showQRCode = async (url: string, base64: string, buffer: Buffer) => {
   await WebUiDataRuntime.setQQLoginQrcodeURL(url);
