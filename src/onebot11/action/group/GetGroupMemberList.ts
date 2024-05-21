7756484a9b5e8067dff9ada7a17da1780121fb9b
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-21 19:27:59 +0800

    feat: GroupMemberDetailInfo

--- src/onebot11/action/group/GetGroupMemberList.ts
+++ src/onebot11/action/group/GetGroupMemberList.ts
@@ -3,7 +3,7 @@ import { OB11GroupMember } from '../../types';
 import { OB11Constructor } from '../../constructor';
 import BaseAction from '../BaseAction';
 import { ActionName } from '../types';
-import { napCatCore, NTQQGroupApi } from '@/core';
+import { napCatCore, NTQQGroupApi, NTQQUserApi } from '@/core';
 import { WebApi } from '@/core/apis/webapi';
 import { logDebug } from '@/common/utils/log';
 import { FromSchema, JSONSchema } from 'json-schema-to-ts';
@@ -29,14 +29,15 @@ class GetGroupMemberList extends BaseAction<Payload, OB11GroupMember[]> {
       throw (`群${payload.group_id}不存在`);
     }
     if (payload.no_cache == true || payload.no_cache === 'true') {
-      // webGroupMembers = await WebApi.getGroupMembers(payload.group_id.toString());
-      const _groupMembers = await NTQQGroupApi.getGroupMembers(payload.group_id.toString());
+      // webGroupMembers = await WebApi.getGroupMembers(payload.group_id.toString());'
+      const _groupMembers = await NTQQGroupApi.getGroupMembers(payload.group_id.toString(), true);
       groupMembers.set(group.groupCode, _groupMembers);
     }
     const _groupMembers = OB11Constructor.groupMembers(group);
 
     // 方便索引处理
     for (let i = 0, len = _groupMembers.length; i < len; i++) {
+
       MemberMap.set(_groupMembers[i].user_id, _groupMembers[i]);
     }
     // 合并数据
@@ -46,6 +47,7 @@ class GetGroupMemberList extends BaseAction<Payload, OB11GroupMember[]> {
       }
       const MemberData = MemberMap.get(webGroupMembers[i]?.uin);
       if (MemberData) {
+        MemberData.card = webGroupMembers[i].card;
         MemberData.join_time = webGroupMembers[i]?.join_time;
         MemberData.last_sent_time = webGroupMembers[i]?.last_speak_time;
         MemberData.qage = webGroupMembers[i]?.qage;
