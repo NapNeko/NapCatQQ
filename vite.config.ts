5c3305d8fa6adb9f617e0086f4555a6fb49af692
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-23 18:10:18 +0800

    Revert "feat: try support LiteLoader"
    This reverts commit 8101d17482e7af5a9d19dcd43506ff2576758634.

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+// import PreprocessorDirectives from 'unplugin-preprocessor-directives/vite';
 import obfuscator from 'rollup-plugin-obfuscator';
 import cp from 'vite-plugin-cp';
 import { UserConfig, defineConfig } from 'vite';
