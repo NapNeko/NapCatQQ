17ed1e0dc8b8b9201b2a9588d9ff10101eca6191
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-25 12:48:13 +0800

    fix

--- src/onebot11/action/file/GetOnlineClient.ts
+++ src/onebot11/action/file/GetOnlineClient.ts
@@ -1,7 +1,7 @@
 import BaseAction from '../BaseAction';
 import { ActionName } from '../types';
 export class GetOnlineClient extends BaseAction<void, Array<any>> {
-  actionName = ActionName.GetRobotUinRange;
+  actionName = ActionName.GetOnlineClient;
 
   protected async _handle(payload: void) {
 
