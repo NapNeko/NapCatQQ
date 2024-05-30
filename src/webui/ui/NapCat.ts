5e8ca7629c3139baf06eeca863873bd19258ced6
手瓜一十雪 <nanaeonn@outlook.com>
2024-05-30 11:01:34 +0800

    build: 1.4.2-fix:file list

--- src/webui/ui/NapCat.ts
+++ src/webui/ui/NapCat.ts
@@ -29,7 +29,7 @@ async function onSettingWindowCreated(view: Element) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           undefined,
-          SettingButton('V1.4.1', 'napcat-update-button', 'secondary')
+          SettingButton('V1.4.2', 'napcat-update-button', 'secondary')
         ),
       ]),
       SettingList([
