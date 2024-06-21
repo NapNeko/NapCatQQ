185f167c5f485b1023b94b22719e9e186e612e4d
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-21 11:21:09 +0800

    build: 1.5.9 - refactor cache

--- src/onebot11/action/group/GetGroupList.ts
+++ src/onebot11/action/group/GetGroupList.ts
@@ -2,7 +2,6 @@ import { OB11Group } from '../../types';
 import { OB11Constructor } from '../../constructor';
 import BaseAction from '../BaseAction';
 import { ActionName } from '../types';
-import { groups } from '@/core/data';
 import { NTQQGroupApi } from '@/core/apis';
 import { Group } from '@/core/entities';
 import { log } from '@/common/utils/log';
@@ -21,7 +20,7 @@ class GetGroupList extends BaseAction<Payload, OB11Group[]> {
   actionName = ActionName.GetGroupList;
   PayloadSchema = SchemaData;
   protected async _handle(payload: Payload) {
-    let groupList: Group[] = Array.from(groups.values());
+    let groupList: Group[] = await NTQQGroupApi.getGroups();
     if (groupList.length === 0 || payload?.no_cache === true || payload.no_cache === 'true') {
       groupList = await NTQQGroupApi.getGroups(true);
       // log('get groups', groups);
