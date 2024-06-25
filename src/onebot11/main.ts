1cf14ed1a7cf42956f0a2622d01feb5f99a5d1b2
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-25 17:24:42 +0800

    fix: 还原修改

--- src/onebot11/main.ts
+++ src/onebot11/main.ts
@@ -209,8 +209,8 @@ export class NapCatOnebot11 {
       //console.log('ob11 onRecvMsg', JSON.stringify(msg, null, 2));
       // logDebug('收到消息', msg);
       for (const m of msg) {
-        // try: 减掉3s 试图修复消息半天收不到
-        if (this.bootTime - 3 > parseInt(m.msgTime)) {
+        // try: 减掉3s 试图修复消息半天收不到（不减了不减了 会出大问题）
+        if (this.bootTime > parseInt(m.msgTime)) {
           logDebug(`消息时间${m.msgTime}早于启动时间${this.bootTime}，忽略上报`);
           continue;
         }
