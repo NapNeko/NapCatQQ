6075d98eaa1b82aeac1ffb3595a680d2eb48f9ec
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-26 18:09:41 +0800

    release: v1.6.2

--- src/onebot11/action/group/SetGroupLeave.ts
+++ src/onebot11/action/group/SetGroupLeave.ts
@@ -3,6 +3,7 @@ import { ActionName } from '../types';
 import { NTQQGroupApi } from '@/core/apis/group';
 import { log, logError } from '@/common/utils/log';
 import { FromSchema, JSONSchema } from 'json-schema-to-ts';
+import { deleteGroup } from '@/core/data';
 const SchemaData = {
   type: 'object',
   properties: {
@@ -19,6 +20,7 @@ export default class SetGroupLeave extends BaseAction<Payload, any> {
   protected async _handle(payload: Payload): Promise<any> {
     try {
       await NTQQGroupApi.quitGroup(payload.group_id.toString());
+      deleteGroup(payload.group_id.toString());
     } catch (e) {
       logError('退群失败', e);
       throw e;
--- src/onebot11/main.ts
+++ src/onebot11/main.ts
@@ -276,7 +276,7 @@ export class NapCatOnebot11 {
       const isPrivilege = role === 3 || role === 4;
       for (const member of members.values()) {
         //console.log(member?.isDelete, role, isPrivilege);
-        if (member?.isDelete && !isPrivilege /*&& selfInfo.uin !== member.uin*/) {
+        if (member?.isDelete && !isPrivilege && selfInfo.uin !== member.uin) {
           log('[群聊] 群组 ', groupCode, ' 成员' + member.uin + '退出');
           const groupDecreaseEvent = new OB11GroupDecreaseEvent(parseInt(groupCode), parseInt(member.uin), 0, 'leave');// 不知道怎么出去的
           postOB11Event(groupDecreaseEvent, true);
