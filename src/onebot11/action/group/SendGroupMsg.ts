a643fac073e146ec8c866ba24d380ebf196a9de6
手瓜一十雪 <nanaeonn@outlook.com>
2024-06-28 13:38:03 +0800

    refactor: msg context

--- src/onebot11/action/group/SendGroupMsg.ts
+++ src/onebot11/action/group/SendGroupMsg.ts
@@ -1,10 +1,11 @@
-import SendMsg from '../msg/SendMsg';
+import SendMsg, { ContextMode } from '../msg/SendMsg';
 import { ActionName, BaseCheckResult } from '../types';
 import { OB11PostSendMsg } from '../../types';
 
 // 未检测参数
 class SendGroupMsg extends SendMsg {
   actionName = ActionName.SendGroupMsg;
+  contextMode: ContextMode = ContextMode.Group;
 
   protected async check(payload: OB11PostSendMsg): Promise<BaseCheckResult> {
     delete payload.user_id;
