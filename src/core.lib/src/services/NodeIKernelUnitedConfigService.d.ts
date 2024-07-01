65c25adfb7da7090ada93f83b7af6a87d46795a9
手瓜一十雪 <nanaeonn@outlook.com>
2024-07-01 18:24:36 +0800

    refactor: napcat

--- src/core.lib/src/services/NodeIKernelUnitedConfigService.d.ts
+++
@@ -1,9 +0,0 @@
-export interface NodeIKernelUnitedConfigService {
-    addKernelUnitedConfigListener(...args: any[]): unknown;
-    removeKernelUnitedConfigListener(...args: any[]): unknown;
-    fetchUnitedCommendConfig(...args: any[]): unknown;
-    fetchUnitedSwitchConfig(...args: any[]): unknown;
-    loadUnitedConfig(...args: any[]): unknown;
-    isUnitedConfigSwitchOn(...args: any[]): unknown;
-    registerUnitedConfigPushGroupList(...args: any[]): unknown;
-}
