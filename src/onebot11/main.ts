71213c24211f5d7c68ef43ca755d22edcf33fb6d
手瓜一十雪 <nanaeonn@outlook.com>
2024-07-11 12:31:00 +0800

    build: 移除调试代码

--- src/onebot11/main.ts
+++ src/onebot11/main.ts
@@ -332,7 +332,6 @@ export class NapCatOnebot11 {
         postOB11Event(msg);
         // log("post msg", msg)
       }).catch(e => logError('constructMessage error: ', e));
-      await OB11Constructor.GroupEvent(message);
       OB11Constructor.GroupEvent(message).then(groupEvent => {
         if (groupEvent) {
           // log("post group event", groupEvent);
