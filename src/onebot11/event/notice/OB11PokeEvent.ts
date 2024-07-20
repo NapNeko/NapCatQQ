8d22f1851918beaf107292259bde50db6eac30dd
Nepenthe <Ttjonne@outlook.com>
2024-07-20 14:51:50 +0800

    群戳一戳增加原始信息 群消息log增加视频解析

--- src/onebot11/event/notice/OB11PokeEvent.ts
+++ src/onebot11/event/notice/OB11PokeEvent.ts
@@ -19,11 +19,13 @@ export class OB11FriendPokeEvent extends OB11PokeEvent {
 
 export class OB11GroupPokeEvent extends OB11PokeEvent {
   group_id: number;
+  raw_message: any;
 
-  constructor(group_id: number, user_id: number = 0, target_id: number = 0,) {
+  constructor(group_id: number, user_id: number = 0, target_id: number = 0, raw_message: any) {
     super();
     this.group_id = group_id;
     this.target_id = target_id;
     this.user_id = user_id;
+    this.raw_message = raw_message;
   }
 }
