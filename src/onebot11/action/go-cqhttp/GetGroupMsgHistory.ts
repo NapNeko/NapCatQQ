be605f11f25aeb495d8c2d5d85d8089cc828f426
Alen <cnxysoft@126.com>
2024-07-27 15:01:08 +0800

    修复提交疏漏
    修改在查询群历史消息时，如未提供msg_seq，则返回最新消息

--- src/onebot11/action/go-cqhttp/GetGroupMsgHistory.ts
+++ src/onebot11/action/go-cqhttp/GetGroupMsgHistory.ts
@@ -18,7 +18,7 @@ const SchemaData = {
     message_seq: { type: 'number' },
     count: { type: 'number' }
   },
-  required: ['group_id', 'count']
+  required: [ 'group_id' ]
 } as const satisfies JSONSchema;
 
 type Payload = FromSchema<typeof SchemaData>;
@@ -31,13 +31,16 @@ export default class GoCQHTTPGetGroupMsgHistory extends BaseAction<Payload, Resp
     if (!group) {
       throw `群${payload.group_id}不存在`;
     }
-    const startMsgId = (await MessageUnique.getMsgIdAndPeerByShortId(payload.message_seq || 0))?.MsgId || '0';
-    // log("startMsgId", startMsgId)
-    const historyResult = (await NTQQMsgApi.getMsgHistory({
+    let targetMsgShortId, count = parseInt(payload.count?.toString() ?? '20');
+    const peer = {
       chatType: ChatType.group,
       peerUid: group.groupCode
-    }, startMsgId, parseInt(payload.count?.toString()) || 20));
-    //logDebug(historyResult);
+    }
+    if ( !payload.message_seq ) {
+      targetMsgShortId = await MessageUnique.getNthLatestShortIdByPeer(peer, count)
+    }
+    const startMsgId = (await MessageUnique.getMsgIdAndPeerByShortId(targetMsgShortId ?? (payload.message_seq ?? 0)))?.MsgId || '0';
+    const historyResult = (await NTQQMsgApi.getMsgHistory(peer, startMsgId, count));
     const msgList = historyResult.msgList;
     await Promise.all(msgList.map(async msg => {
       msg.id = await MessageUnique.createMsg({ guildId: '', chatType: msg.chatType, peerUid: msg.peerUid }, msg.msgId);
