1d0973bd031b9e96fd9eb44b4b2e29a464f4b4a4
手瓜一十雪 <nanaeonn@outlook.com>
2024-07-31 22:21:23 +0800

    release: 1.7.3

--- src/webui/ui/NapCat.ts
+++ src/webui/ui/NapCat.ts
@@ -29,7 +29,7 @@ async function onSettingWindowCreated(view: Element) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           undefined,
-          SettingButton('V1.7.2', 'napcat-update-button', 'secondary')
+          SettingButton('V1.7.3', 'napcat-update-button', 'secondary')
         ),
       ]),
       SettingList([
