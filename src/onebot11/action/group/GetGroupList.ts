31824c0504167d58d31ededae15b21313e95594a
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-04 20:56:49 +0800

    refactor: get_group_list

--- src/onebot11/action/group/GetGroupList.ts
+++ src/onebot11/action/group/GetGroupList.ts
@@ -2,10 +2,8 @@ import { OB11Group } from '../../types';
 import { OB11Constructor } from '../../constructor';
 import BaseAction from '../BaseAction';
 import { ActionName } from '../types';
-import { groups } from '@/core/data';
 import { NTQQGroupApi } from '@/core/apis';
 import { Group } from '@/core/entities';
-import { log } from '@/common/utils/log';
 import { FromSchema, JSONSchema } from 'json-schema-to-ts';
 // no_cache get时传字符串
 const SchemaData = {
@@ -21,11 +19,7 @@ class GetGroupList extends BaseAction<Payload, OB11Group[]> {
   actionName = ActionName.GetGroupList;
   PayloadSchema = SchemaData;
   protected async _handle(payload: Payload) {
-    let groupList: Group[] = Array.from(groups.values());
-    if (groupList.length === 0 || payload?.no_cache === true || payload.no_cache === 'true') {
-      groupList = await NTQQGroupApi.getGroups(true);
-      // log('get groups', groups);
-    }
+    let groupList: Group[]  = await NTQQGroupApi.getGroups(payload?.no_cache === true || payload.no_cache === 'true');
     return OB11Constructor.groups(groupList);
   }
 }
