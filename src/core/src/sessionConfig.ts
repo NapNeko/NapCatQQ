6b83a901514865d60e9ef7a9d811176b8613d67e
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-04 14:15:40 +0800

    chore: 订正类型

--- src/core/src/sessionConfig.ts
+++ src/core/src/sessionConfig.ts
@@ -34,42 +34,42 @@ export interface WrapperSessionInitConfig {
     account_path: string // 可以通过NodeQQNTWrapperUtil().getNTUserDataInfoConfig()获取
   }
   clientVer: string  // 9.9.8-22355
-  a2: '',
-  d2: '',
-  d2Key: '',
-  machineId: '',
-  platform: 3,  // 3是Windows?
+  a2: string,
+  d2: string,
+  d2Key: string,
+  machineId: string,
+  platform: PlatformType,  // 3是Windows?
   platVer: string,  // 系统版本号, 应该可以固定
   appid: string,
   rdeliveryConfig: {
-    appKey: '',
-    systemId: 0,
-    appId: '',
-    logicEnvironment: '',
-    platform: 3,
-    language: '',
-    sdkVersion: '',
-    userId: '',
-    appVersion: '',
-    osVersion: '',
-    bundleId: '',
-    serverUrl: '',
-    fixedAfterHitKeys: ['']
+    appKey: string,
+    systemId: number,
+    appId: string,
+    logicEnvironment: string,
+    platform: PlatformType,
+    language: string,
+    sdkVersion: string,
+    userId: string,
+    appVersion: string,
+    osVersion: string,
+    bundleId: string,
+    serverUrl: string,
+    fixedAfterHitKeys: string[]
   }
-  'defaultFileDownloadPath': string, // 这个可以通过环境变量获取？
-  'deviceInfo': {
-    'guid': string,
-    'buildVer': string,
-    'localId': 2052,
-    'devName': string,
-    'devType': string,
-    'vendorName': '',
-    'osVer': string,
-    'vendorOsName': string,
-    'setMute': false,
-    'vendorType': 0
+  defaultFileDownloadPath: string, // 这个可以通过环境变量获取？
+  deviceInfo: {
+    guid: string,
+    buildVer: string,
+    localId: number,
+    devName: string,
+    devType: string,
+    vendorName: string,
+    osVer: string,
+    vendorOsName: string,
+    setMute: boolean,
+    vendorType: VendorType
   },
-  'deviceConfig': '{"appearance":{"isSplitViewMode":true},"msg":{}}'
+  deviceConfig: '{"appearance":{"isSplitViewMode":true},"msg":{}}'
 }
 
 export const sessionConfig: WrapperSessionInitConfig | any = {};
@@ -92,7 +92,7 @@ export async function genSessionConfig(selfUin: string, selfUid: string, account
     d2: '',
     d2Key: '',
     machineId: '',
-    platform: 3,  // 3是Windows?
+    platform: PlatformType.KWINDOWS,  // 3是Windows?
     platVer: systemVersion,  // 系统版本号, 应该可以固定
     appid: QQVersionAppid,
     rdeliveryConfig: {
@@ -100,7 +100,7 @@ export async function genSessionConfig(selfUin: string, selfUid: string, account
       systemId: 0,
       appId: '',
       logicEnvironment: '',
-      platform: 3,
+      platform: PlatformType.KWINDOWS,
       language: '',
       sdkVersion: '',
       userId: '',
@@ -121,11 +121,10 @@ export async function genSessionConfig(selfUin: string, selfUid: string, account
       osVer: systemVersion,
       vendorOsName: systemName,
       setMute: false,
-      vendorType: 0
+      vendorType: VendorType.KNOSETONIOS
     },
-    'deviceConfig': '{"appearance":{"isSplitViewMode":true},"msg":{}}'
+    deviceConfig: '{"appearance":{"isSplitViewMode":true},"msg":{}}'
   };
   Object.assign(sessionConfig, config);
-  // log(sessionConfig);
   return config;
 }
