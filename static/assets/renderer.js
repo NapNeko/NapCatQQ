a272feda6ae3c3c5aab187e0220e8a808258c043
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-05 13:52:34 +0800

    release: 1.7.8

--- static/assets/renderer.js
+++ static/assets/renderer.js
@@ -163,7 +163,7 @@ async function onSettingWindowCreated(view) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           void 0,
-          SettingButton("V1.7.7", "napcat-update-button", "secondary")
+          SettingButton("V1.7.8", "napcat-update-button", "secondary")
         )
       ]),
       SettingList([
