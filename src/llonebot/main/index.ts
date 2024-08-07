a3af19e1e68aeacef7389cbb630d6da248309bfc
Wesley F. Young <willamwei@outlook.com>
2024-08-07 16:41:31 +0800

    feat: run as ll plugin

--- src/llonebot/main/index.ts
+++ src/llonebot/main/index.ts
@@ -1,4 +1,39 @@
-import { BrowserWindow } from 'electron';
+import { injectService, LoginListener, NodeIKernelLoginService } from '@/core';
+import { NodeIQQNTWrapperSession, WrapperNodeApi } from '@/core/wrapper';
+import { fetchServices } from '@/llonebot/main/proxy';
+import { INapCatService } from '@/core';
+import { InitWebUi } from '@/webui';
+import { NapCatOnebot11 } from '@/onebot11/main';
+import { WebUiDataRuntime } from '@/webui/src/helper/Data';
 
-export const onBrowserWindowCreated = (window: BrowserWindow) => {
-};
+class NapCatLLPluginImpl extends INapCatService {
+  constructor(session: NodeIQQNTWrapperSession, wrapper: WrapperNodeApi, loginService: NodeIKernelLoginService) {
+    super(session, wrapper);
+    const ntLoginListener = new LoginListener();
+    ntLoginListener.onQRCodeLoginSucceed = arg => {
+      this.onLoginSuccessFuncList.forEach(func => func(arg.uin, arg.uid));
+    };
+    loginService.addKernelLoginListener(
+      new wrapper.NodeIKernelLoginListener(
+        new Proxy(ntLoginListener, this.proxyHandler)
+      )
+    );
+  }
+}
+
+async function init() {
+  const {
+    wrapperSession,
+    wrapperNodeApi,
+    wrapperLoginService
+  } = await fetchServices();
+
+  const service = new NapCatLLPluginImpl(wrapperSession, wrapperNodeApi, wrapperLoginService);
+  injectService(service);
+
+  await InitWebUi();
+  const NapCat_OneBot11 = new NapCatOnebot11();
+  await WebUiDataRuntime.setOB11ConfigCall(NapCat_OneBot11.SetConfig);
+}
+
+init();
