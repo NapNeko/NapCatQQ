1ec855a9881a99a7254fc76cd7d9850ae0443fba
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-07 17:18:58 +0800

    chore: webui

--- src/liteloader/renderer/index.ts
+++ src/liteloader/renderer/index.ts
@@ -2,4 +2,8 @@ export const onSettingWindowCreated = (view: HTMLElement) => {
     //创建iframe
     const iframe = document.createElement('iframe');
     iframe.src = 'https://127.0.0.1:6099/webui?key=';
+    iframe.style.width = '100%';
+    iframe.style.height = '100%';
+    iframe.style.border = 'none';
+    view.appendChild(iframe);
 };
