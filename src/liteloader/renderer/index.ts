3301264687d4f4c0910031a148df33c1271a13d7
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-07 19:55:06 +0800

    fix

--- src/liteloader/renderer/index.ts
+++ src/liteloader/renderer/index.ts
@@ -1,7 +1,7 @@
 export const onSettingWindowCreated = (view: HTMLElement) => {
     //创建iframe
     const iframe = document.createElement('iframe');
-    iframe.src = 'https://127.0.0.1:6099/webui?key=';
+    iframe.src = 'http://127.0.0.1:6099/webui/';
     iframe.style.width = '100%';
     iframe.style.height = '100%';
     iframe.style.border = 'none';
