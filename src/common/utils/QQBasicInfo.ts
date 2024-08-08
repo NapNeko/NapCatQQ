5ae9be0291b2d03d38c1df4cf43b7bd1ef894013
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-08 14:36:59 +0800

    chore: 补全基础框架

---
+++ src/common/utils/QQBasicInfo.ts
@@ -0,0 +1,57 @@
+import path from 'node:path';
+import fs from 'node:fs';
+import { systemPlatform } from '@/common/utils/system';
+import { getDefaultQQVersionConfigInfo, getQQVersionConfigPath } from './helper';
+import AppidTable from '@/core/external/appid.json';
+import { log } from './log';
+
+//基础目录获取
+export const QQMainPath = process.execPath;
+export const QQPackageInfoPath: string = path.join(path.dirname(QQMainPath), 'resources', 'app', 'package.json');
+export const QQVersionConfigPath: string | undefined = getQQVersionConfigPath(QQMainPath);
+
+//基础信息获取 无快更则启用默认模板填充
+export const isQuickUpdate: boolean = !!QQVersionConfigPath;
+export const QQVersionConfig: QQVersionConfigType = isQuickUpdate ? JSON.parse(fs.readFileSync(QQVersionConfigPath!).toString()) : getDefaultQQVersionConfigInfo();
+export const QQPackageInfo: QQPackageInfoType = JSON.parse(fs.readFileSync(QQPackageInfoPath).toString());
+export const { appid: QQVersionAppid, qua: QQVersionQua } = getAppidV2();
+
+//基础函数
+export function getQQBuildStr() {
+  return isQuickUpdate ? QQVersionConfig.buildId : QQPackageInfo.buildVersion;
+}
+export function getFullQQVesion() {
+  return isQuickUpdate ? QQVersionConfig.curVersion : QQPackageInfo.version;
+}
+export function requireMinNTQQBuild(buildStr: string) {
+  return parseInt(getQQBuildStr()) >= parseInt(buildStr);
+}
+//此方法不要直接使用
+export function getQUAInternal() {
+  return systemPlatform === 'linux' ? `V1_LNX_NQ_${getFullQQVesion()}_${getQQBuildStr()}_GW_B` : `V1_WIN_NQ_${getFullQQVesion()}_${getQQBuildStr()}_GW_B`;
+}
+export function getAppidV2(): { appid: string, qua: string } {
+  const appidTbale = AppidTable as unknown as QQAppidTableType;
+  try {
+    const data = appidTbale[getFullQQVesion()];
+    if (data) {
+      return data;
+    }
+  }
+  catch (e) {
+    log(`[QQ版本兼容性检测] 获取Appid异常 请检测NapCat/QQNT是否正常`);
+  }
+  // 以下是兜底措施
+  log(`[QQ版本兼容性检测] ${getFullQQVesion()} 版本兼容性不佳，可能会导致一些功能无法正常使用`);
+  return { appid: systemPlatform === 'linux' ? '537237950' : '537237765', qua: getQUAInternal() };
+}
+// platform_type: 3,
+// app_type: 4,
+// app_version: '9.9.12-25765',
+// qua: 'V1_WIN_NQ_9.9.12_25765_GW_B',
+// appid: '537234702',
+// platVer: '10.0.26100',
+// clientVer: '9.9.9-25765',
+// Linux
+// app_version: '3.2.9-25765',
+// qua: 'V1_LNX_NQ_3.2.10_25765_GW_B',
--- src/core/core.ts
+++ src/core/core.ts
@@ -1,3 +1,5 @@
+import { logDebug } from "@/common/utils/log";
+import { NodeIKernelLoginService } from "./services";
 import { NodeIQQNTWrapperSession } from "./wrapper/wrapper";
 
 export enum NCoreWorkMode {
@@ -9,6 +11,19 @@ export class NapCatCore {
     public WorkMode: NCoreWorkMode = NCoreWorkMode.Unknown;
     public isInit: boolean = false;
     public session: NodeIQQNTWrapperSession | undefined;
+    private proxyHandler = {
+        get(target: any, prop: any, receiver: any) {
+          // console.log('get', prop, typeof target[prop]);
+          if (typeof target[prop] === 'undefined') {
+            // 如果方法不存在，返回一个函数，这个函数调用existentMethod
+            return (...args: unknown[]) => {
+              logDebug(`${target.constructor.name} has no method ${prop}`);
+            };
+          }
+          // 如果方法存在，正常返回
+          return Reflect.get(target, prop, receiver);
+        }
+      };
     get IsInit(): boolean {
         return this.isInit;
     }
@@ -16,12 +31,12 @@ export class NapCatCore {
 export class NapCatShell extends NapCatCore {
     public WorkMode: NCoreWorkMode = NCoreWorkMode.Shell;
     Init() {
-
+        
     }
 }
 export class NapCatLiteLoader extends NapCatCore {
     public WorkMode: NCoreWorkMode = NCoreWorkMode.LiteLoader;
-    Init(LoginService: any, WrapperSession: any) {
+    Init(WrapperSession: NodeIQQNTWrapperSession, LoginService: NodeIKernelLoginService) {
 
     }
 }
