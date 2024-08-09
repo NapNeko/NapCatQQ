c271a4b2cb9452623ac90f1ac82c4fa2269a82b8
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-09 22:14:45 +0800

    fix

--- src/onebot/main.ts
+++ src/onebot/main.ts
@@ -1,12 +1,16 @@
 import { NapCatCore, InstanceContext } from "@/core";
+import { OB11Config } from "./helper/config";
+import { NapCatPathWrapper } from "@/common/framework/napcat";
 
 //OneBot实现类
 export class NapCatOneBot11Adapter {
     readonly core: NapCatCore;
     readonly context: InstanceContext;
+    config: OB11Config;
 
-    constructor(core: NapCatCore, context: InstanceContext) {
+    constructor(core: NapCatCore, context: InstanceContext, pathWrapper: NapCatPathWrapper) {
         this.core = core;
         this.context = context;
+        this.config = new OB11Config(core, pathWrapper.configPath);
     }
 }
