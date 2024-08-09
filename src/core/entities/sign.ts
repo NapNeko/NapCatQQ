855df4094d06dbf97d238ed52bb6cf965598a335
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-09 14:24:48 +0800

    fix: signApi

---
+++ src/core/entities/sign.ts
@@ -0,0 +1,23 @@
+export interface IdMusicSignPostData {
+    type: 'qq' | '163',
+    id: string | number,
+}
+
+export interface CustomMusicSignPostData {
+    type: 'custom',
+    url: string,
+    audio: string,
+    title: string,
+    image?: string,
+    singer?: string
+}
+export interface MiniAppLuaJsonType {
+    prompt: string,
+    title: string,
+    preview: string,
+    jumpUrl: string,
+    tag: string,
+    tagIcon: string,
+    source: string,
+    sourcelogo: string,
+  }
