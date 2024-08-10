ba2235e2f6ebca4c73357edcdaf4db9ccb84873d
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-10 18:06:40 +0800

    chore: 核心缓存位置设计完成

--- src/onebot/api/friend.ts
+++ src/onebot/api/friend.ts
@@ -4,6 +4,7 @@ import { NapCatOneBot11Adapter } from "../main";
 export class OneBotFriendApi {
     obContext: NapCatOneBot11Adapter;
     coreContext: NapCatCore;
+    friendList: Map<string, any> = new Map();//此处作为缓存 uin->info
     constructor(obContext: NapCatOneBot11Adapter, coreContext: NapCatCore) {
         this.obContext = obContext;
         this.coreContext = coreContext;
--- src/onebot/api/group.ts
+++ src/onebot/api/group.ts
@@ -1,8 +1,9 @@
 import { NapCatCore } from "@/core";
 import { NapCatOneBot11Adapter } from "../main";
-export class OneBotGroupApi{
+export class OneBotGroupApi {
     obContext: NapCatOneBot11Adapter;
     coreContext: NapCatCore;
+    GroupMemberList: Map<string, any> = new Map();//此处作为缓存 group_id->memberUin->info
     constructor(obContext: NapCatOneBot11Adapter, coreContext: NapCatCore) {
         this.obContext = obContext;
         this.coreContext = coreContext;
