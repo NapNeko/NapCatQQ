86fc7935e5f1f82cb0c17ae51a8c95ff30bc50c3
Wesley F. Young <willamwei@outlook.com>
2024-08-12 00:15:51 +0800

    fix: core config name

--- src/core/helper/config.ts
+++ src/core/helper/config.ts
@@ -1,6 +1,7 @@
 import { ConfigBase } from "@/common/utils/ConfigBase";
 import { LogLevel } from "@/common/utils/log";
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export interface NapCatConfig {
     fileLog: boolean,
     consoleLog: boolean,
@@ -8,9 +9,9 @@ export interface NapCatConfig {
     consoleLogLevel: LogLevel,
 }
 
+// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class NapCatConfig extends ConfigBase<NapCatConfig> {
     getConfigName() {
-        return 'onebot11';
+        return 'napcat';
     }
-
 }
