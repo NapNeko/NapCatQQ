a5adc554cdb8cd7ef5efa6f087b076cabbb2a7b7
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-12 00:35:35 +0800

    chore: 日志

--- src/common/utils/ConfigBase.ts
+++ src/common/utils/ConfigBase.ts
@@ -32,20 +32,20 @@ export abstract class ConfigBase<T> {
         if (!fs.existsSync(configPath)) {
             try {
                 fs.writeFileSync(configPath, fs.readFileSync(this.getConfigPath(undefined), 'utf-8'));
-                logger.log(`配置文件${configPath}创建成功!\n`);
+                logger.log(`[Core] [Config] 配置文件创建成功!\n`);
             } catch (e: any) {
-                logger.logError(`创建配置文件 ${configPath} 时发生错误:`, e.message);
+                logger.logError(`[Core] [Config] 创建配置文件时发生错误:`, e.message);
             }
         }
         try {
             this.configData = JSON.parse(fs.readFileSync(configPath, 'utf-8'));
-            logger.logDebug(`配置文件${configPath}已加载`, this.configData);
+            logger.logDebug(`[Core] [Config] 配置文件${configPath}加载`, this.configData);
             return this.configData;
         } catch (e: any) {
             if (e instanceof SyntaxError) {
-                logger.logError(`配置文件 ${configPath} 格式错误，请检查配置文件:`, e.message);
+                logger.logError(`[Core] [Config] 配置文件格式错误，请检查配置文件:`, e.message);
             } else {
-                logger.logError(`读取配置文件 ${configPath} 时发生错误:`, e.message);
+                logger.logError(`[Core] [Config] 读取配置文件时发生错误:`, e.message);
             }
             return {} as T;
         }
