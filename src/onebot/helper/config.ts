436249597d0693c27c8aeacea8063c002e877f8a
Wesley F. Young <willamwei@outlook.com>
2024-08-12 01:08:55 +0800

    fix: use constructor to pass config name

--- src/onebot/helper/config.ts
+++ src/onebot/helper/config.ts
@@ -1,10 +1,11 @@
 import { ConfigBase } from '@/common/utils/ConfigBase';
 import ob11DefaultConfig from '@/onebot/external/onebot11.json';
+import { NapCatCore } from '@/core';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export type OB11Config = typeof ob11DefaultConfig;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class OB11ConfigLoader extends ConfigBase<OB11Config> {
-    name = 'onebot11';
+    constructor(coreContext: NapCatCore, configPath: string) {
+        super('onebot11', coreContext, configPath);
+    }
 }
