fd226c45f66a234fe2d637d80167a7ea4806f863
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-12 12:41:59 +0800

    chore: Support 多账户

--- src/framework/liteloader.cjs
+++ src/framework/liteloader.cjs
@@ -1,7 +1,6 @@
 //LiteLoader需要提供部分IPC接口，以便于其他插件调用
 const { ipcMain } = require('electron');
-const fs = require('fs');
+const napcat= require('./napcat.cjs');
 ipcMain.handle("napcat_get_webtoken", async (event, arg) => {
-    return "http://127.0.0.1:6099/webui/?token=" + JSON.parse(fs.readFileSync(__dirname + '/config/webui.json', 'utf-8').toString()).token;
+    return napcat.NCgetWebUiUrl();
 });
-require('./napcat.cjs');
