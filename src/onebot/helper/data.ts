1c8a80857132e3e6ca6bc8c92638ad07a0f0ff60
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-12 17:16:41 +0800

    chore: 2.0.4

--- src/onebot/helper/data.ts
+++ src/onebot/helper/data.ts
@@ -408,7 +408,7 @@ export class OB11Constructor {
             return;
         }
         //log("group msg", msg);
-        // Mlikiowa V2.0.2 Refactor Todo
+        // Mlikiowa V2.0.4 Refactor Todo
         // if (msg.senderUin && msg.senderUin !== '0') {
         //   const member = await getGroupMember(msg.peerUid, msg.senderUin);
         //   if (member && member.cardName !== msg.sendMemberName) {
