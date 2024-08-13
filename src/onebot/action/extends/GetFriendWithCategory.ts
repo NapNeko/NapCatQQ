516838c521481b56a1472e5c7a6a830f93d90691
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-14 00:47:31 +0800

    style&&chore: 逐步清退老版本代码

--- src/onebot/action/extends/GetFriendWithCategory.ts
+++ src/onebot/action/extends/GetFriendWithCategory.ts
@@ -6,14 +6,9 @@ export class GetFriendWithCategory extends BaseAction<void, any> {
     actionName = ActionName.GetFriendsWithCategory;
 
     async _handle(payload: void) {
-        if (this.CoreContext.context.basicInfoWrapper.requireMinNTQQBuild('26702')) {
-            //全新逻辑
-            return (await this.CoreContext.apis.FriendApi.getBuddyV2ExWithCate(true)).map(category => ({
-                ...category,
-                buddyList: OB11Constructor.friendsV2(category.buddyList),
-            }));
-        } else {
-            throw new Error('this ntqq version not support, must be 26702 or later');
-        }
+        return (await this.CoreContext.apis.FriendApi.getBuddyV2ExWithCate(true)).map(category => ({
+            ...category,
+            buddyList: OB11Constructor.friendsV2(category.buddyList),
+        }));
     }
 }
--- src/onebot/action/extends/GetRobotUinRange.ts
+++ src/onebot/action/extends/GetRobotUinRange.ts
@@ -5,7 +5,6 @@ export class GetRobotUinRange extends BaseAction<void, Array<any>> {
     actionName = ActionName.GetRobotUinRange;
 
     async _handle(payload: void) {
-        // console.log(await NTQQUserApi.getRobotUinRange());
         const NTQQUserApi = this.CoreContext.apis.UserApi;
         return await NTQQUserApi.getRobotUinRange();
     }
