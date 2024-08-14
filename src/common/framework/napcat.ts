50579bb9e655c50d48fe505455f66024a2ce452f
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-14 10:46:59 +0800

    fix: 修复某个笨蛋写的

--- src/common/framework/napcat.ts
+++ src/common/framework/napcat.ts
@@ -2,7 +2,7 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-export const napcat_version = '2.0.18';
+export const napcat_version = '2.0.19';
 
 export class NapCatPathWrapper {
     binaryPath: string;
