f1e6663d665cc55ab7c4f98e935cdbb5f2916adf
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-14 19:38:50 +0800

    release: 2.0.20

--- src/common/framework/napcat.ts
+++ src/common/framework/napcat.ts
@@ -2,7 +2,7 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
 
-export const napcat_version = '2.0.19';
+export const napcat_version = '2.0.20';
 
 export class NapCatPathWrapper {
     binaryPath: string;
