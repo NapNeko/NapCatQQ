c4d7d5a0d40ea210d82f9491047b99f444700a53
Alen <cnxysoft@126.com>
2024-08-20 01:02:03 +0800

    fix: 多处修改
    1.修改（疑似）旧设备回复消息验证失败的解决方案
    2.修复Base64发送文件失败
    3.修复群时间监听器未注册

--- src/onebot/helper/data.ts
+++ src/onebot/helper/data.ts
@@ -149,7 +149,6 @@ export class OB11Constructor {
                 message_data['type'] = OB11MessageDataType.reply;
                 //log("收到回复消息", element.replyElement);
                 try {
-                    let oldMsgFlag = false;
                     const records = msg.records.find(msgRecord => msgRecord.msgId === element?.replyElement?.sourceMsgIdInRecords);
                     const peer = {
                         chatType: msg.chatType,
@@ -164,14 +163,17 @@ export class OB11Constructor {
                         chatType: msg.chatType,
                     }, element.replyElement.replayMsgSeq, 1, true, true)).msgList.find(msg => msg.msgRandom === records.msgRandom);
                     if (!replyMsg || records.msgRandom !== replyMsg.msgRandom) {
-                        if (!replyMsg && records.msgRandom === '0') oldMsgFlag = true;
                         replyMsg = (await NTQQMsgApi.getSingleMsg(peer, element.replyElement.replayMsgSeq)).msgList[0];
                     }
                     if (msg.peerUin == '284840486') {
                         //合并消息内侧 消息具体定位不到
                     }
-                    if ((!replyMsg || (records.msgRandom !== replyMsg.msgRandom && !oldMsgFlag || (oldMsgFlag && records.msgSeq !== replyMsg.msgSeq))) && msg.peerUin !== '284840486') {
-                        throw new Error('回复消息消息验证失败');
+                    if ((!replyMsg || records.msgRandom !== replyMsg.msgRandom) && msg.peerUin !== '284840486') {
+                        const replyMsgList = (await NTQQMsgApi.getMsgExBySeq(peer, records.msgSeq)).msgList;
+                        if (replyMsgList.length < 1) {
+                            throw new Error('回复消息消息验证失败');
+                        }
+                        replyMsg = replyMsgList.filter(e => e.msgSeq == records.msgSeq).sort((a, b) => parseInt(a.msgTime) - parseInt(b.msgTime))[0];
                     }
                     message_data['data']['id'] = MessageUnique.createMsg({
                         peerUid: msg.peerUid,
