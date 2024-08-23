424a5cc14322a4f3d32a238d7235f2a37cad1f7a
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-23 10:55:19 +0800

    release: 2.2.0

--- src/framework/napcat.cjs
+++ src/framework/napcat.cjs
@@ -1,82 +1,120 @@
-// https://github.com/NapNeko/LiteLoader-NapCatExample/blob/main/src/common/proxy.ts
-// By Mlikiowa
+// 用于 NapCat 对QQ流程分析
+let Process = require('process');
+let os = require('os');
+let path = require('path');
+Process.dlopenOrig = Process.dlopen;
 
-const process = require('process');
-const os = require('os');
-const path = require('path');
+let RealWrapper;
+let loginService;
 
+//Fuck LoginService
+class LoginService {
+    constructor() {
+        console.log('[NapCat] Fuck LoginService Loading...');
+    }
+}
+
+//NapCat 专有逻辑
+let initCallBack;
+let wrapperSession;
+let wrapperLoginService;
 const currentPath = path.dirname(__filename);
 
-const dlopenOrig = process.dlopen;
+function CreateFuckService(ServiceName) {
+    return new Proxy(() => { }, {
+        get: function (target, FunctionName, receiver) {
+            console.log("Proxy... Event:", ServiceName + '/' + FunctionName);
+            if (ServiceName == 'NodeIKernelLoginService' && FunctionName == 'get') {
+                return function () {
+                    let ret = new Proxy(new LoginService(), {
+                        get: function (target, ClassFunName, receiver) {
+                            return function () {
+                                let ret = loginService[ClassFunName](...arguments);
+                                // if (ret instanceof Promise) {
+                                //     ret.then((data) => {
+                                //         console.log("Called ", '实例方法 NodeIKernelLoginService/' + ClassFunName, ' 参数:', ...arguments, '返回:', data);
+                                //     });
+                                // } else {
+                                //     console.log("Called ", '实例方法 NodeIKernelLoginService/' + ClassFunName, ' 参数:', ...arguments, '返回:', ret);
+                                // }
+                                return ret;
+                            }
 
-let wrapperSession;
-let wrapperNodeApi;
-let wrapperLoginService;
-let initCallBack;
+                        }
+                    })
+                    return ret;
+                }
+            }
 
-// Proxy dlopen
-process.dlopen = (module, filename, flags = os.constants.dlopen.RTLD_LAZY) => {
-    dlopenOrig(module, filename, flags);
-    for (const export_name in module.exports) {
-        module.exports[export_name] = new Proxy(module.exports[export_name], {
-            construct: (target, args, _newTarget) => {
-                let constructed;
-                if (export_name === 'NodeIKernelSessionListener') {
-                    let HookedArg = [];
-                    for (let ArgIndex in args) {
-                        if (args[ArgIndex] instanceof Object) {
-                            let HookArg = {};
-                            for (let ListenerName in args[ArgIndex]) {
-                                HookArg[ListenerName] = function (...ListenerData) {
-                                    try {
-                                        if (ListenerName === 'onSessionInitComplete') {
-                                            //回调成功
-                                            initCallBack.forEach((cb) => cb(...ListenerData));
+            if (ServiceName == 'NodeIQQNTWrapperSession' && FunctionName == 'create') {
+                return new Proxy(() => { }, {
+                    apply: function (target, thisArg, argArray) {
+                        let Session = RealWrapper.NodeIQQNTWrapperSession[FunctionName](...argArray);
+                        //传递Session
+                        wrapperSession = Session;
+
+                        let ret = new Proxy(Session, {
+                            get: function (target, ClassFunName, receiver) {
+                                return function () {
+                                    if (ClassFunName == 'init') {
+                                        let origin = arguments[3].onSessionInitComplete;
+                                        arguments[3].onSessionInitComplete = function () {
+                                            //console.log("Listner ", '注册方法 NodeIKernelSessionListener/onSessionInitComplete', ' 参数:', ...arguments);
+                                            origin(...arguments);
+                                            initCallBack.forEach((cb) => cb(...arguments));
                                             clearHook();
                                         }
-                                        //console.log("Construct-ARG-Apply", ListenerName, JSON.stringify(ListenerData, null, 2));
-                                    } catch (error) {
-                                        // ignored
                                     }
-                                    args[ArgIndex][ListenerName](...ListenerData);
-                                };
-                                HookedArg.push(HookArg);
+                                    let ret = Session[ClassFunName](...arguments);
+                                    // if (ret instanceof Promise) {
+                                    //     ret.then((data) => {
+                                    //         console.log("Called ", '实例方法 NodeIQQNTWrapperSession/' + ClassFunName, ' 参数:', ...arguments, '返回:', data);
+                                    //     });
+                                    // } else {
+                                    //     console.log("Called ", '实例方法 NodeIQQNTWrapperSession/' + ClassFunName, ' 参数:', ...arguments, '返回:', ret);
+                                    // }
+
+                                    return ret;
+                                }
+
                             }
-                        } else {
-                            // 其它类型
-                            //console.log("Construct-ARG-NotProxy", args[keyArg]);
-                        }
-
+                        });
+                        return ret;
                     }
-                    constructed = new target(...HookedArg);
-                } else {
-                    constructed = new target(...args);
-                }
-
-                if (export_name === 'NodeIQQNTWrapperSession') wrapperSession = constructed;
-                if (export_name === 'NodeIKernelLoginService') wrapperLoginService = constructed;
-
-                return constructed;
-            },
-        });
+                });
+            }
+        }
     }
-    if (filename.toLowerCase().includes('wrapper.node')) {
-        wrapperNodeApi = module.exports;
-    }
+    );
+}
+Process.dlopen = function (module, filename, flags = os.constants.dlopen.RTLD_LAZY) {
+    let dlopenRet = this.dlopenOrig(module, filename, flags);
+    if (filename.indexOf('wrapper.node') == -1) return dlopenRet;
+    //仅对Wrapper.node进行处理
+    RealWrapper = module.exports;
+    //先行获取LoginService操作权
+    loginService = new RealWrapper.NodeIKernelLoginService();
+    wrapperLoginService = loginService;
+    //开始针对性处理
+    module.exports = new Proxy({}, {
+        get: function (target, ServiceName, receiver) {
+            if (ServiceName == 'NodeIKernelLoginService') return CreateFuckService(ServiceName);
+            if (ServiceName == 'NodeIQQNTWrapperSession') return CreateFuckService(ServiceName);
+            return RealWrapper[ServiceName];
+        }
+    });
+    //返回预先构造的对象
+    return dlopenRet;
 };
 
-/**
- * 清理 Hook
- */
+//辅助函数
 function clearHook() {
     initCallBack = [];
     process.dlopen = dlopenOrig;
 }
 
 function ntIsInitialized_Internal() {
-    return wrapperSession !== undefined
-        && wrapperNodeApi !== undefined
-        && wrapperLoginService !== undefined;
+    return wrapperSession !== undefined;
 }
 
 function pollForNTInitializationCheck() {
@@ -111,7 +149,7 @@ async function fetchServices(timeout = 10000) {
             setTimeout(() => resolve(false), timeout);
         }),
     ]).then(result => result ?
-        { wrapperSession, wrapperNodeApi, wrapperLoginService } :
+        { wrapperSession, wrapperLoginService } :
         Promise.reject("fetchServices Timeout!"),
     );
 }
--- static/assets/renderer.js
+++ static/assets/renderer.js
@@ -164,7 +164,7 @@ async function onSettingWindowCreated(view) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           void 0,
-          SettingButton("V2.1.0", "napcat-update-button", "secondary")
+          SettingButton("V2.2.0", "napcat-update-button", "secondary")
         )
       ]),
       SettingList([
