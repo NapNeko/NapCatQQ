444af844086e46866906904eff6cc48221b51108
Seijo Cecilia <willamwei@outlook.com>
2024-08-25 19:38:35 +0800

    refactor: ob11 to raw message constructors

--- src/onebot/action/msg/SendMsg/index.ts
+++ src/onebot/action/msg/SendMsg/index.ts
@@ -9,11 +9,9 @@ import { ActionName, BaseCheckResult } from '@/onebot/action/types';
 import fs from 'node:fs';
 import fsPromise from 'node:fs/promises';
 import { decodeCQCode } from '@/onebot/helper/cqcode';
-import createSendElements from './create-send-elements';
 import { MessageUnique } from '@/common/utils/MessageUnique';
-import { ChatType, ElementType, NapCatCore, Peer, SendMessageElement } from '@/core';
+import { ChatType, ElementType, NapCatCore, Peer, RawMessage, SendMessageElement } from '@/core';
 import BaseAction from '../../BaseAction';
-import { handleForwardNode } from './handle-forward-node';
 
 export interface ReturnDataType {
     message_id: number;
@@ -33,8 +31,6 @@ export function normalize(message: OB11MessageMixType, autoEscape = false): OB11
             decodeCQCode(message)
     ) : Array.isArray(message) ? message : [message];
 }
-
-export { createSendElements };
 
 export async function sendMsg(coreContext: NapCatCore, peer: Peer, sendElements: SendMessageElement[], deleteAfterSentFiles: string[], waitComplete = true) {
     const NTQQMsgApi = coreContext.apis.MsgApi;
@@ -144,9 +140,6 @@ export class SendMsg extends BaseAction<OB11PostSendMsg, ReturnDataType> {
     contextMode = ContextMode.Normal;
 
     protected async check(payload: OB11PostSendMsg): Promise<BaseCheckResult> {
-        const NTQQGroupApi = this.CoreContext.apis.GroupApi;
-        const NTQQFriendApi = this.CoreContext.apis.FriendApi;
-        const NTQQUserApi = this.CoreContext.apis.UserApi;
         const messages = normalize(payload.message);
         const nodeElementLength = getSpecialMsgNum(payload, OB11MessageDataType.node);
         if (nodeElementLength > 0 && nodeElementLength != messages.length) {
@@ -156,9 +149,9 @@ export class SendMsg extends BaseAction<OB11PostSendMsg, ReturnDataType> {
             };
         }
         if (payload.user_id && payload.message_type !== 'group') {
-            const uid = await NTQQUserApi.getUidByUinV2(payload.user_id.toString());
-            const isBuddy = await NTQQFriendApi.isBuddy(uid!);
-            //if (!isBuddy) { }
+            // const uid = await this.CoreContext.apis.UserApi.getUidByUinV2(payload.user_id.toString());
+            // const isBuddy = await NTQQFriendApi.isBuddy(uid!);
+            // if (!isBuddy) { }
         }
         return { valid: true };
     }
@@ -174,7 +167,7 @@ export class SendMsg extends BaseAction<OB11PostSendMsg, ReturnDataType> {
         );
 
         if (getSpecialMsgNum(payload, OB11MessageDataType.node)) {
-            const returnMsg = await handleForwardNode(this.CoreContext, this.OneBotContext, peer, messages as OB11MessageNode[]);
+            const returnMsg = await this.handleForwardNode(peer, messages as OB11MessageNode[]);
             if (returnMsg) {
                 const msgShortId = MessageUnique.createMsg({
                     guildId: '',
@@ -194,10 +187,133 @@ export class SendMsg extends BaseAction<OB11PostSendMsg, ReturnDataType> {
         }
         // log("send msg:", peer, sendElements)
 
-        const { sendElements, deleteAfterSentFiles } = await createSendElements(this.CoreContext, this.OneBotContext, messages, peer);
-        //console.log(peer, JSON.stringify(sendElements,null,2));
+        const { sendElements, deleteAfterSentFiles } = await this.OneBotContext.apiContext.MsgApi
+            .createSendElements(messages, peer);
         const returnMsg = await sendMsg(this.CoreContext, peer, sendElements, deleteAfterSentFiles);
         return { message_id: returnMsg!.id! };
+    }
+
+    private async handleForwardNode(destPeer: Peer, messageNodes: OB11MessageNode[]): Promise<RawMessage | null> {
+        const NTQQMsgApi = this.CoreContext.apis.MsgApi;
+        const selfPeer = {
+            chatType: ChatType.KCHATTYPEC2C,
+            peerUid: this.CoreContext.selfInfo.uid,
+        };
+        let nodeMsgIds: string[] = [];
+        const logger = this.CoreContext.context.logger;
+        for (const messageNode of messageNodes) {
+            const nodeId = messageNode.data.id;
+            if (nodeId) {
+                //对Mgsid和OB11ID混用情况兜底
+                const nodeMsg = MessageUnique.getMsgIdAndPeerByShortId(parseInt(nodeId)) || MessageUnique.getPeerByMsgId(nodeId);
+                if (!nodeMsg) {
+                    logger.logError('转发消息失败，未找到消息', nodeId);
+                    continue;
+                }
+                nodeMsgIds.push(nodeMsg.MsgId);
+            } else {
+                // 自定义的消息
+                try {
+                    const OB11Data = normalize(messageNode.data.content);
+                    //筛选node消息
+                    const isNodeMsg = OB11Data.filter(e => e.type === OB11MessageDataType.node).length;//找到子转发消息
+                    if (isNodeMsg !== 0) {
+                        if (isNodeMsg !== OB11Data.length) {
+                            logger.logError('子消息中包含非node消息 跳过不合法部分');
+                            continue;
+                        }
+                        const nodeMsg = await this.handleForwardNode(selfPeer, OB11Data.filter(e => e.type === OB11MessageDataType.node));
+                        if (nodeMsg) {
+                            nodeMsgIds.push(nodeMsg.msgId);
+                            MessageUnique.createMsg(selfPeer, nodeMsg.msgId);
+                        }
+                        //完成子卡片生成跳过后续
+                        continue;
+                    }
+                    const { sendElements } = await this.OneBotContext.apiContext.MsgApi
+                        .createSendElements(OB11Data, destPeer);
+                    //拆分消息
+                    const MixElement = sendElements.filter(element => element.elementType !== ElementType.FILE && element.elementType !== ElementType.VIDEO);
+                    const SingleElement = sendElements.filter(element => element.elementType === ElementType.FILE || element.elementType === ElementType.VIDEO).map(e => [e]);
+                    const AllElement: SendMessageElement[][] = [MixElement, ...SingleElement].filter(e => e !== undefined && e.length !== 0);
+                    const MsgNodeList: Promise<RawMessage | undefined>[] = [];
+                    for (const sendElementsSplitElement of AllElement) {
+                        MsgNodeList.push(sendMsg(this.CoreContext, selfPeer, sendElementsSplitElement, [], true).catch(_ => new Promise((resolve) => {
+                            resolve(undefined);
+                        })));
+                    }
+                    (await Promise.allSettled(MsgNodeList)).map((result) => {
+                        if (result.status === 'fulfilled' && result.value) {
+                            nodeMsgIds.push(result.value.msgId);
+                            MessageUnique.createMsg(selfPeer, result.value.msgId);
+                        }
+                    });
+                } catch (e) {
+                    logger.logDebug('生成转发消息节点失败', e);
+                }
+            }
+        }
+        const nodeMsgArray: Array<RawMessage> = [];
+        let srcPeer: Peer | undefined = undefined;
+        let needSendSelf = false;
+        //检测是否处于同一个Peer 不在同一个peer则全部消息由自身发送
+        for (const msgId of nodeMsgIds) {
+            const nodeMsgPeer = MessageUnique.getPeerByMsgId(msgId);
+            if (!nodeMsgPeer) {
+                logger.logError('转发消息失败，未找到消息', msgId);
+                continue;
+            }
+            const nodeMsg = (await NTQQMsgApi.getMsgsByMsgId(nodeMsgPeer.Peer, [msgId])).msgList[0];
+            srcPeer = srcPeer ?? { chatType: nodeMsg.chatType, peerUid: nodeMsg.peerUid };
+            if (srcPeer.peerUid !== nodeMsg.peerUid) {
+                needSendSelf = true;
+            }
+            nodeMsgArray.push(nodeMsg);
+        }
+        nodeMsgIds = nodeMsgArray.map(msg => msg.msgId);
+        let retMsgIds: string[] = [];
+        if (needSendSelf) {
+            for (const msg of nodeMsgArray) {
+                if (msg.peerUid === this.CoreContext.selfInfo.uid) continue;
+                const ClonedMsg = await this.cloneMsg(msg);
+                if (ClonedMsg) retMsgIds.push(ClonedMsg.msgId);
+            }
+        } else {
+            retMsgIds = nodeMsgIds;
+        }
+        if (nodeMsgIds.length === 0) throw Error('转发消息失败，生成节点为空');
+        try {
+            logger.logDebug('开发转发', srcPeer, destPeer, nodeMsgIds);
+            return await NTQQMsgApi.multiForwardMsg(srcPeer!, destPeer, nodeMsgIds);
+        } catch (e) {
+            logger.logError('forward failed', e);
+            return null;
+        }
+    }
+
+    async cloneMsg(msg: RawMessage): Promise<RawMessage | undefined> {
+        const selfPeer = {
+            chatType: ChatType.KCHATTYPEC2C,
+            peerUid: this.CoreContext.selfInfo.uid,
+        };
+        const logger = this.CoreContext.context.logger;
+        const NTQQMsgApi = this.CoreContext.apis.MsgApi;
+        //logDebug('克隆的目标消息', msg);
+
+        const sendElements: SendMessageElement[] = [];
+
+        for (const element of msg.elements) {
+            sendElements.push(element as SendMessageElement);
+        }
+
+        if (sendElements.length === 0) {
+            logger.logDebug('需要clone的消息无法解析，将会忽略掉', msg);
+        }
+        try {
+            return await NTQQMsgApi.sendMsg(selfPeer, sendElements, true);
+        } catch (e) {
+            logger.logError(e, '克隆转发消息失败,将忽略本条消息', msg);
+        }
     }
 }
 
