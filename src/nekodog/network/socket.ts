27ae33135267ab21d92db8246038e87a99bb0bb2
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-27 20:37:23 +0800

    chore: new adapter

---
+++ src/nekodog/network/socket.ts
@@ -0,0 +1,18 @@
+import { createServer } from 'node:net';
+export class NewAdapterNetwork {
+    constructor(public host: number, public port: number) { }
+    async open() {
+        const server = createServer((socket) => {
+            socket.on('data', (data) => {
+
+            });
+            socket.on('end', () => {
+
+            });
+            socket.on('connect', () => {
+
+            })
+        });
+        server.listen(this.port, this.host);
+    }
+}
