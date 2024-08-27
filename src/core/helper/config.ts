027ffbffa60e5d85771fdc4d3a7fe77a25eac8de
Wesley F. Young <willamwei@outlook.com>
2024-08-27 10:05:23 +0800

    chore: remove redundant eslint-disable

--- src/core/helper/config.ts
+++ src/core/helper/config.ts
@@ -2,10 +2,8 @@ import { ConfigBase } from '@/common/utils/config-base';
 import napCatDefaultConfig from '@/core/external/napcat.json';
 import { NapCatCore } from '@/core';
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export type NapCatConfig = typeof napCatDefaultConfig;
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-declaration-merging
 export class NapCatConfigLoader extends ConfigBase<NapCatConfig> {
     constructor(core: NapCatCore, configPath: string) {
         super('napcat', core, configPath);
