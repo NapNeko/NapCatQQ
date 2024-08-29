fc3a8481bc34cf501099d904c8ff408aa1bbe4ef
手瓜一十雪 <nanaeonn@outlook.com>
2024-08-29 19:32:37 +0800

    fix: #320

--- src/onebot/event/notice/OB11GroupEssenceEvent.ts
+++ src/onebot/event/notice/OB11GroupEssenceEvent.ts
@@ -5,11 +5,14 @@ export class OB11GroupEssenceEvent extends OB11GroupNoticeEvent {
     notice_type = 'essence';
     message_id: number;
     sender_id: number;
+    operator_id: number;
     sub_type: 'add' | 'delete' = 'add';
 
-    constructor(core: NapCatCore, groupId: number, message_id: number, sender_id: number) {
+
+    constructor(core: NapCatCore, groupId: number, message_id: number, sender_id: number, operator_id: number) {
         super(core, groupId, sender_id);
         this.group_id = groupId;
+        this.operator_id = operator_id;
         this.message_id = message_id;
         this.sender_id = sender_id;
     }
