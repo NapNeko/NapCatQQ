a91b5b964f219c8238704c940043e679c42fbb71
Wesley F. Young <willamwei@outlook.com>
2024-08-31 23:43:01 +0800

    release: 2.2.30

--- src/webui/ui/NapCat.ts
+++ src/webui/ui/NapCat.ts
@@ -30,7 +30,7 @@ async function onSettingWindowCreated(view: Element) {
                 SettingItem(
                     '<span id="napcat-update-title">Napcat</span>',
                     undefined,
-                    SettingButton('V2.2.29', 'napcat-update-button', 'secondary'),
+                    SettingButton('V2.2.30', 'napcat-update-button', 'secondary'),
                 ),
             ]),
             SettingList([
