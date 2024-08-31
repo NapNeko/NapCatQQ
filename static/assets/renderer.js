4c8329c573cdc72b9c3c19f4808b2157d797bbcf
Wesley F. Young <willamwei@outlook.com>
2024-08-31 10:35:33 +0800

    release: 2.2.28

--- static/assets/renderer.js
+++ static/assets/renderer.js
@@ -164,7 +164,7 @@ async function onSettingWindowCreated(view) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           void 0,
-          SettingButton("V2.2.27", "napcat-update-button", "secondary")
+          SettingButton("V2.2.28", "napcat-update-button", "secondary")
         )
       ]),
       SettingList([
