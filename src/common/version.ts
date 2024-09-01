1dfe89025a90ddd1a0812c36387f93acbb608020
手瓜一十雪 <nanaeonn@outlook.com>
2024-09-01 16:13:41 +0800

    release: 2.2.32

--- src/common/version.ts
+++ src/common/version.ts
@@ -1,1 +1,1 @@
-export const napCatVersion = '2.2.31';
+export const napCatVersion = '2.2.32';
--- src/webui/ui/NapCat.ts
+++ src/webui/ui/NapCat.ts
@@ -30,7 +30,7 @@ async function onSettingWindowCreated(view: Element) {
                 SettingItem(
                     '<span id="napcat-update-title">Napcat</span>',
                     undefined,
-                    SettingButton('V2.2.31', 'napcat-update-button', 'secondary'),
+                    SettingButton('V2.2.32', 'napcat-update-button', 'secondary'),
                 ),
             ]),
             SettingList([
