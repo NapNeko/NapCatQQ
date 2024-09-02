35a59c26d5e7899d231db2061db4e752c11fbca1
Wesley F. Young <willamwei@outlook.com>
2024-09-02 12:31:30 +0800

    feat: action handler definition

---
+++ src/laana-v0.1.1/action/index.ts
@@ -0,0 +1,19 @@
+import { ActionPing, ActionPong } from '@/laana-v0.1.1/types/action/wrapper';
+
+type ExtractFromPongOrVoid<key> = Extract<ActionPong['pong'], { oneofKind: key; }> extends never ?
+    void :
+    // eslint-disable-next-line
+    // @ts-ignore
+    Extract<ActionPong['pong'], { oneofKind: key; }>[key];
+
+type LaanaActionMapping = {
+    [key in Exclude<ActionPing['ping']['oneofKind'], undefined>]:
+    (
+        params:
+            // eslint-disable-next-line
+            // @ts-ignore
+            Extract<ActionPing['ping'], { oneofKind: key; }>[key]
+    ) => PromiseLike<ExtractFromPongOrVoid<key>>;
+};
+
+export type LaanaActionHandler = Partial<LaanaActionMapping>;
