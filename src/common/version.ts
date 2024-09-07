2609e06edd686302b61f5b17b8302bc8ed146567
手瓜一十雪 <nanaeonn@outlook.com>
2024-09-07 13:31:32 +0800

    release: 2.3.6

--- src/common/version.ts
+++ src/common/version.ts
@@ -1,1 +1,1 @@
-export const napCatVersion = '2.3.5';
+export const napCatVersion = '2.3.6';
--- static/assets/renderer.js
+++ static/assets/renderer.js
@@ -164,7 +164,7 @@ async function onSettingWindowCreated(view) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           void 0,
-          SettingButton("V2.3.5", "napcat-update-button", "secondary")
+          SettingButton("V2.3.6", "napcat-update-button", "secondary")
         )
       ]),
       SettingList([
