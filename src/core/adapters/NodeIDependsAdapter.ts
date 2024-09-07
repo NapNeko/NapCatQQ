a299d9c0a765fa939af8c63ef6180e686a672e33
手瓜一十雪 <nanaeonn@outlook.com>
2024-09-07 22:58:29 +0800

    refactor: adapter

--- src/core/adapters/NodeIDependsAdapter.ts
+++ src/core/adapters/NodeIDependsAdapter.ts
@@ -1,9 +1,8 @@
+import { MsfChangeReasonType, MsfStatusType } from "../entities/adapter";
+
 export class NodeIDependsAdapter {
-    onMSFStatusChange(arg1: number, arg2: number) {
-        // console.log(arg1, arg2);
-        // if (arg1 == 2 && arg2 == 2) {
-        //   log("NapCat丢失网络连接,请检查网络")
-        // }
+    onMSFStatusChange(statusType: MsfStatusType, changeReasonType: MsfChangeReasonType) {
+
     }
 
     onMSFSsoError(args: unknown) {
