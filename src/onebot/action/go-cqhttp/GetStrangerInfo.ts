8ee31bce3ccb10b4fdadbe26e3954b111e93afdf
手瓜一十雪 <nanaeonn@outlook.com>
2024-09-13 13:54:12 +0800

    fix

--- src/onebot/action/go-cqhttp/GetStrangerInfo.ts
+++ src/onebot/action/go-cqhttp/GetStrangerInfo.ts
@@ -21,24 +21,24 @@ export default class GoCQHTTPGetStrangerInfo extends BaseAction<Payload, OB11Use
     async _handle(payload: Payload): Promise<OB11User> {
         const user_id = payload.user_id.toString();
         const extendData = await this.core.apis.UserApi.getUserDetailInfoByUin(user_id);
-        const uid = (await this.core.apis.UserApi.getUidByUinV2(user_id))!;
-        if (!uid || uid.indexOf('*') != -1) {
-            return {
-                ...extendData.detail.simpleInfo.coreInfo,
-                ...extendData.detail.commonExt,
-                ...extendData.detail.simpleInfo.baseInfo,
-                ...extendData.detail.simpleInfo.relationFlags,
-                ...extendData.detail.simpleInfo.status,
-                user_id: parseInt(extendData.detail.uin) || 0,
-                nickname: extendData.detail.simpleInfo.coreInfo.nick,
-                sex: OB11UserSex.unknown,
-                age: extendData.detail.simpleInfo.baseInfo.age || 0,
-                qid: extendData.detail.simpleInfo.baseInfo.qid,
-                level: calcQQLevel(extendData.detail.commonExt?.qqLevel ?? 0) || 0,
-                login_days: 0,
-            };
-        }
-        const data = { ...extendData, ...(await this.core.apis.UserApi.getUserDetailInfo(uid)) };
-        return OB11Entities.stranger(data);
+        let uid = (await this.core.apis.UserApi.getUidByUinV2(user_id));
+        if (!uid) uid = extendData.detail.uid;
+        const info = (await this.core.apis.UserApi.getUserDetailInfo(uid));
+
+        return {
+            user_id: parseInt(extendData.detail.uin) ?? 0,
+            nickname: extendData.detail.simpleInfo.coreInfo.nick,
+            age: extendData.detail.simpleInfo.baseInfo.age ?? info.age,
+            qid: extendData.detail.simpleInfo.baseInfo.qid,
+            qqLevel: calcQQLevel(extendData.detail.commonExt?.qqLevel ?? info.qqLevel),
+            sex: OB11Entities.sex(extendData.detail.simpleInfo.baseInfo.sex) ?? OB11UserSex.unknown,
+            long_nick: extendData.detail.simpleInfo.baseInfo.longNick ?? info.longNick,
+            reg_time: extendData.detail.commonExt.regTime ?? info.regTime,
+            is_vip: extendData.detail.simpleInfo.vasInfo?.svipFlag,
+            is_years_vip: extendData.detail.simpleInfo.vasInfo?.yearVipFlag,
+            vip_level: extendData.detail.simpleInfo.vasInfo?.vipLevel,
+            remark: extendData.detail.simpleInfo.coreInfo.remark ?? info.remark,
+            status: extendData.detail.simpleInfo.status?.status ?? info.status
+        };
     }
 }
