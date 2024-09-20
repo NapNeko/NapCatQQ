8cc45b8292299bb1614f0f054eacedb6dae814f5
Fripine <Fripine@hotmail.com>
2024-09-20 10:21:26 +0800

    chore

--- src/onebot/api/msg.ts
+++ src/onebot/api/msg.ts
@@ -632,18 +632,10 @@ export class OneBotMsgApi {
             return;
         }
         for (const element of msg.elements) {
-            if (element.grayTipElement) {
-                if (element.grayTipElement.subElementType == NTGrayTipElementSubTypeV2.GRAYTIP_ELEMENT_SUBTYPE_JSON) {
-                    if (element.grayTipElement.jsonGrayTipElement.busiId == 1061) {
-                        const PokeEvent = await this.obContext.apis.FriendApi.parsePrivatePokeEvent(element.grayTipElement);
-                        if (PokeEvent) return PokeEvent;
-                    }
-                }
-                if (element.grayTipElement.subElementType == NTGrayTipElementSubTypeV2.GRAYTIP_ELEMENT_SUBTYPE_XMLMSG) {
-                    //好友添加成功事件
-                    if (element.grayTipElement.xmlElement.templId === '10229' && msg.peerUin !== '') {
-                        return new OB11FriendAddNoticeEvent(this.core, parseInt(msg.peerUin) || Number(await this.core.apis.UserApi.getUinByUidV2(msg.peerUid)));
-                    }
+            if (element.grayTipElement && element.grayTipElement.subElementType == NTGrayTipElementSubTypeV2.GRAYTIP_ELEMENT_SUBTYPE_JSON) {
+                if (element.grayTipElement.jsonGrayTipElement.busiId == 1061) {
+                    const PokeEvent = await this.obContext.apis.FriendApi.parsePrivatePokeEvent(element.grayTipElement);
+                    if (PokeEvent) return PokeEvent;
                 }
             }
         }
