ef979aeeaadaad003165c433fdf8bc8c2210c003
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-12 21:35:49 +0800

    style: NapProto

--- src/core/proto/NapProto.ts
+++ src/core/proto/NapProto.ts
@@ -1,5 +1,5 @@
-import {MessageType, RepeatType, ScalarType} from '@protobuf-ts/runtime';
-import {PartialFieldInfo} from "@protobuf-ts/runtime/build/types/reflection-info";
+import { MessageType, RepeatType, ScalarType } from '@protobuf-ts/runtime';
+import { PartialFieldInfo } from "@protobuf-ts/runtime/build/types/reflection-info";
 
 type LowerCamelCase<S extends string> = CamelCaseHelper<S, false, true>;
 
@@ -9,23 +9,23 @@ type CamelCaseHelper<
     IsFirstChar extends boolean
 > = S extends `${infer F}${infer R}`
     ? F extends '_'
-        ? CamelCaseHelper<R, true, false>
-        : F extends `${number}`
-            ? `${F}${CamelCaseHelper<R, true, false>}`
-            : CapNext extends true
-                ? `${Uppercase<F>}${CamelCaseHelper<R, false, false>}`
-                : IsFirstChar extends true
-                    ? `${Lowercase<F>}${CamelCaseHelper<R, false, false>}`
-                    : `${F}${CamelCaseHelper<R, false, false>}`
+    ? CamelCaseHelper<R, true, false>
+    : F extends `${number}`
+    ? `${F}${CamelCaseHelper<R, true, false>}`
+    : CapNext extends true
+    ? `${Uppercase<F>}${CamelCaseHelper<R, false, false>}`
+    : IsFirstChar extends true
+    ? `${Lowercase<F>}${CamelCaseHelper<R, false, false>}`
+    : `${F}${CamelCaseHelper<R, false, false>}`
     : '';
 
 type ScalarTypeToTsType<T extends ScalarType> =
     T extends ScalarType.DOUBLE | ScalarType.FLOAT | ScalarType.INT32 | ScalarType.FIXED32 | ScalarType.UINT32 | ScalarType.SFIXED32 | ScalarType.SINT32 ? number :
-        T extends ScalarType.INT64 | ScalarType.UINT64 | ScalarType.FIXED64 | ScalarType.SFIXED64 | ScalarType.SINT64 ? bigint :
-            T extends ScalarType.BOOL ? boolean :
-                T extends ScalarType.STRING ? string :
-                    T extends ScalarType.BYTES ? Uint8Array :
-                        never;
+    T extends ScalarType.INT64 | ScalarType.UINT64 | ScalarType.FIXED64 | ScalarType.SFIXED64 | ScalarType.SINT64 ? bigint :
+    T extends ScalarType.BOOL ? boolean :
+    T extends ScalarType.STRING ? string :
+    T extends ScalarType.BYTES ? Uint8Array :
+    never;
 
 interface BaseProtoFieldType<T, O extends boolean, R extends O extends true ? false : boolean> {
     kind: 'scalar' | 'message';
@@ -55,25 +55,25 @@ function ProtoField<T extends ScalarType, O extends boolean = false, R extends O
 function ProtoField<T extends () => ProtoMessageType, O extends boolean = false, R extends O extends true ? false : boolean = false>(no: number, type: T, repeated?: R, optional?: O): MessageProtoFieldType<T, O, R>;
 function ProtoField(no: number, type: ScalarType | (() => ProtoMessageType), repeated?: boolean, optional?: boolean): ProtoFieldType {
     if (typeof type === 'function') {
-        return {kind: 'message', no: no, type: type, repeated: repeated ?? false, optional: optional ?? false};
+        return { kind: 'message', no: no, type: type, repeated: repeated ?? false, optional: optional ?? false };
     } else {
-        return {kind: 'scalar', no: no, type: type, repeated: repeated ?? false, optional: optional ?? false};
+        return { kind: 'scalar', no: no, type: type, repeated: repeated ?? false, optional: optional ?? false };
     }
 }
 
 type ProtoFieldReturnType<T> = T extends ScalarProtoFieldType<infer S, infer R, infer O>
     ? ScalarTypeToTsType<S>
     : T extends MessageProtoFieldType<infer S, infer R, infer O>
-        ? ProtoStructType<ReturnType<S>>
-        : never;
+    ? ProtoStructType<ReturnType<S>>
+    : never;
 
 type RequiredFieldsType<T> = {
     [K in keyof T as T[K] extends {
         optional: true
     } | MessageProtoFieldType<any, any, any> ? never : LowerCamelCase<K & string>]
     : T[K] extends { repeated: true }
-        ? ProtoFieldReturnType<T[K]>[]
-        : ProtoFieldReturnType<T[K]>
+    ? ProtoFieldReturnType<T[K]>[]
+    : ProtoFieldReturnType<T[K]>
 };
 
 type OptionalFieldsType<T> = {
@@ -81,8 +81,8 @@ type OptionalFieldsType<T> = {
         optional: true
     } | MessageProtoFieldType<any, any, any> ? LowerCamelCase<K & string> : never]?:
     T[K] extends { repeated: true }
-        ? ProtoFieldReturnType<T[K]>[]
-        : ProtoFieldReturnType<T[K]>
+    ? ProtoFieldReturnType<T[K]>[]
+    : ProtoFieldReturnType<T[K]>
 };
 
 type ProtoStructType<T> = RequiredFieldsType<T> & OptionalFieldsType<T>;
@@ -99,17 +99,18 @@ class NapProtoMsg<T extends ProtoMessageType> {
         this._field = Object.keys(fields).map(key => {
             const field = fields[key];
             if (field.kind === 'scalar') {
+                const repeatType = field.repeated
+                    ? [ScalarType.STRING, ScalarType.BYTES].includes(field.type)
+                        ? RepeatType.UNPACKED
+                        : RepeatType.PACKED
+                    : RepeatType.NO;
                 return {
                     no: field.no,
                     name: key,
                     kind: 'scalar',
                     T: field.type,
                     opt: field.optional,
-                    repeat: field.repeated
-                        ? [ScalarType.STRING, ScalarType.BYTES].includes(field.type)
-                            ? RepeatType.UNPACKED
-                            : RepeatType.PACKED
-                        : RepeatType.NO,
+                    repeat: repeatType,
                 };
             } else if (field.kind === 'message') {
                 const rt = NapProtoMsgCache.get(field.type()) ?? (() => {
