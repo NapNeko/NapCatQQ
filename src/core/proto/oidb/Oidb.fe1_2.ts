274c956f161a061ecf5fec874c167b99acf98a20
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-13 11:58:08 +0800

    fix

--- src/core/proto/oidb/Oidb.fe1_2.ts
+++ src/core/proto/oidb/Oidb.fe1_2.ts
@@ -2,8 +2,7 @@ import {ScalarType} from "@protobuf-ts/runtime";
 import {ProtoField} from "../NapProto";
 
 export const OidbSvcTrpcTcp0XFE1_2 = {
-    uid: ProtoField(1, ScalarType.STRING, true),
-    uin: ProtoField(2, ScalarType.UINT32),
+    uin: ProtoField(1, ScalarType.UINT32),
     key: ProtoField(3, () => OidbSvcTrpcTcp0XFE1_2Key, false, true),
 }
 
