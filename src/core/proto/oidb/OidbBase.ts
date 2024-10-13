a0c49d5f7fce78f6fc345a952b70f88591abab65
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-13 10:57:03 +0800

    feat: errorMsg opt

--- src/core/proto/oidb/OidbBase.ts
+++ src/core/proto/oidb/OidbBase.ts
@@ -5,6 +5,6 @@ export const OidbSvcTrpcTcpBase = {
     command: ProtoField(1, ScalarType.UINT32),
     subCommand: ProtoField(2, ScalarType.UINT32),
     body: ProtoField(4, ScalarType.BYTES),
-    errorMsg: ProtoField(5, ScalarType.STRING),
+    errorMsg: ProtoField(5, ScalarType.STRING, true),
     isReserved: ProtoField(12, ScalarType.UINT32)
 }
