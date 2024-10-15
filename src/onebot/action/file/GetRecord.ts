a9201826a6376efdac308a687f1c169c40f75dcf
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-15 08:58:28 +0800

    fix: 临时会话上报问题

--- src/onebot/action/file/GetRecord.ts
+++ src/onebot/action/file/GetRecord.ts
@@ -2,7 +2,7 @@ import { GetFileBase, GetFilePayload, GetFileResponse } from './GetFile';
 import { ActionName } from '../types';
 import { spawn } from 'node:child_process';
 import { promises as fs } from 'fs';
-
+import { decode } from 'silk-wasm';
 const FFMPEG_PATH = process.env.FFMPEG_PATH || 'ffmpeg';
 
 interface Payload extends GetFilePayload {
@@ -16,25 +16,39 @@ export default class GetRecord extends GetFileBase {
         const res = await super._handle(payload);
         if (payload.out_format && typeof payload.out_format === 'string') {
             const inputFile = res.file;
-            const outputFile = `${inputFile}.${payload.out_format}`;
             if (!inputFile) throw new Error('file not found');
+            const pcmFile = `${inputFile}.pcm`;
+            const outputFile = `${inputFile}.${payload.out_format}`;
             try {
                 await fs.access(inputFile);
-                await this.convertFile(inputFile, outputFile, payload.out_format);
+                await this.decodeFile(inputFile, pcmFile);
+                await this.convertFile(pcmFile, outputFile, payload.out_format);
                 const base64Data = await fs.readFile(outputFile, { encoding: 'base64' });
                 res.file = outputFile;
                 res.url = outputFile;
                 res.base64 = base64Data;
             } catch (error) {
                 console.error('Error processing file:', error);
+                throw error; // 重新抛出错误以便调用者可以处理
             }
         }
         return res;
     }
 
+    private async decodeFile(inputFile: string, outputFile: string): Promise<void> {
+        try {
+            const inputData = await fs.readFile(inputFile);
+            const decodedData = await decode(inputData, 24000);
+            await fs.writeFile(outputFile, Buffer.from(decodedData.data));
+        } catch (error) {
+            console.error('Error decoding file:', error);
+            throw error; // 重新抛出错误以便调用者可以处理
+        }
+    }
+
     private convertFile(inputFile: string, outputFile: string, format: string): Promise<void> {
         return new Promise((resolve, reject) => {
-            const ffmpeg = spawn(FFMPEG_PATH, ['-i', inputFile, outputFile]);
+            const ffmpeg = spawn(FFMPEG_PATH, ['-f', 's16le', '-ar', '24000', '-ac', '1', '-i', inputFile, outputFile]);
 
             ffmpeg.on('close', (code) => {
                 if (code === 0) {
