e7222653faa5a769652e0474db4a7d3d3307b11d
pk5ls20 <pk5ls20@outlook.com>
2024-10-20 23:54:21 +0800

    release: 3.0.6

--- static/assets/renderer.js
+++ static/assets/renderer.js
@@ -164,7 +164,7 @@ async function onSettingWindowCreated(view) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           void 0,
-          SettingButton("V3.0.5", "napcat-update-button", "secondary")
+          SettingButton("V3.0.6", "napcat-update-button", "secondary")
         )
       ]),
       SettingList([
