c800a42c65fcd6e014ed50fec2575d3a73b629d7
pk5ls20 <pk5ls20@outlook.com>
2024-10-26 19:51:43 +0800

    fix: payload basic check in `GetPacketStatusDepends`

--- src/onebot/action/packet/GetPacketStatus.ts
+++ src/onebot/action/packet/GetPacketStatus.ts
@@ -5,16 +5,14 @@ import { ActionName, BaseCheckResult } from '../types';
 export abstract class GetPacketStatusDepends<PT, RT> extends BaseAction<PT, RT> {
     actionName = ActionName.GetPacketStatus;
 
-    protected async check(): Promise<BaseCheckResult>{
+    protected async check(payload: PT): Promise<BaseCheckResult>{
         if (!this.core.apis.PacketApi.available) {
             return {
                 valid: false,
                 message: "packetServer不可用，请参照文档 https://napneko.github.io/config/advanced 检查packetServer状态或进行配置！",
             };
         }
-        return {
-            valid: true,
-        };
+        return await super.check(payload);
     }
 }
 
