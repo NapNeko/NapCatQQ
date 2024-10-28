52b3540ec3bdcee772aa66168bab2ec05c3c0925
Nepenthe <Ttjonne@outlook.com>
2024-10-29 07:51:16 +0800

    修改<get_profile_like>接口

--- src/onebot/action/extends/GetProfileLike.ts
+++ src/onebot/action/extends/GetProfileLike.ts
@@ -1,15 +1,22 @@
 import BaseAction from '../BaseAction';
-import { ActionName } from '../types';
+import { ActionName, BaseCheckResult } from '../types';
 
-export class GetProfileLike extends BaseAction<void, any> {
+interface Payload {
+    start: number,
+    count: number
+}
+
+export class GetProfileLike extends BaseAction<Payload, any> {
     actionName = ActionName.GetProfileLike;
 
-    async _handle(payload: void) {
-        const ret = await this.core.apis.UserApi.getProfileLike(this.core.selfInfo.uid);
-        const listdata: any[] = ret.info.userLikeInfos[0].favoriteInfo.userInfos;
+    async _handle(payload: Payload) {
+        const start = payload.start ? Number(payload.start) : 0;
+        const count = payload.count ? Number(payload.count) : 10;
+        const ret = await this.core.apis.UserApi.getProfileLike(this.core.selfInfo.uid, start, count);
+        const listdata: any[] = ret.info.userLikeInfos[0].voteInfo.userInfos;
         for (let i = 0; i < listdata.length; i++) {
             listdata[i].uin = parseInt((await this.core.apis.UserApi.getUinByUidV2(listdata[i].uid)) || '');
         }
-        return listdata;
+        return ret.info.userLikeInfos[0].voteInfo;
     }
 }
