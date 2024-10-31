d8ef97a2defc6efa1a6554c9094318390be6757c
手瓜一十雪 <nanaeonn@outlook.com>
2024-10-31 20:30:02 +0800

    fix: report self

--- src/onebot/index.ts
+++ src/onebot/index.ts
@@ -311,19 +311,19 @@ export class NapCatOneBot11Adapter {
             }
         };
 
-        const msgIdSend = new LRUCache<string, boolean>(100);
+        const msgIdSend = new LRUCache<string, number>(100);
         const recallMsgs = new LRUCache<string, boolean>(100);
         msgListener.onAddSendMsg = async msg => {
             if (msg.sendStatus == SendStatusType.KSEND_STATUS_SENDING) {
-                msgIdSend.put(msg.msgId, true)
+                msgIdSend.put(msg.msgId, 0);
             }
         };
         msgListener.onMsgInfoListUpdate = async msgList => {
             this.emitRecallMsg(msgList, recallMsgs)
                 .catch(e => this.context.logger.logError.bind(this.context.logger)('处理消息失败', e));
             for (const msg of msgList.filter(e => e.senderUin == this.core.selfInfo.uin)) {
-                if (msg.sendStatus == SendStatusType.KSEND_STATUS_SUCCESS && msgIdSend.get(msg.msgId)) {
-                    msgIdSend.put(msg.msgId, true);
+                if (msg.sendStatus == SendStatusType.KSEND_STATUS_SUCCESS && msgIdSend.get(msg.msgId) == 0) {
+                    msgIdSend.put(msg.msgId, 1);
                     // 完成后再post
                     this.apis.MsgApi.parseMessage(msg)
                         .then((ob11Msg) => {
