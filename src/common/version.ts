24e086fe63a1bd56cf745c0f1f95aedae5c8426f
Mlikiowa <nanaeonn@outlook.com>
2024-11-11 12:17:25 +0000

    release: v3.6.11

--- src/common/version.ts
+++ src/common/version.ts
@@ -1,1 +1,1 @@
-export const napCatVersion = '3.6.10';
+export const napCatVersion = '3.6.11';
--- static/assets/renderer.js
+++ static/assets/renderer.js
@@ -164,7 +164,7 @@ async function onSettingWindowCreated(view) {
         SettingItem(
           '<span id="napcat-update-title">Napcat</span>',
           void 0,
-          SettingButton("V3.6.10", "napcat-update-button", "secondary")
+          SettingButton("V3.6.11", "napcat-update-button", "secondary")
         )
       ]),
       SettingList([
