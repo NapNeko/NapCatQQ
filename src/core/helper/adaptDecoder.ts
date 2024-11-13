830b9e3f032f3a64d9b40575dd46ca6c2ccc4070
pk5ls20 <pk5ls20@outlook.com>
2024-11-13 17:44:12 +0800

    feat: sysMessage oldProto adapter

--- src/core/helper/adaptDecoder.ts
+++ src/core/helper/adaptDecoder.ts
@@ -1,36 +1,36 @@
 // TODO: further refactor in NapCat.Packet v2
 import { NapProtoMsg, ProtoField, ScalarType } from "@napneko/nap-proto-core";
 
-export const LikeDetail = {
+const LikeDetail = {
     txt: ProtoField(1, ScalarType.STRING),
     uin: ProtoField(3, ScalarType.INT64),
     nickname: ProtoField(5, ScalarType.STRING)
 };
 
-export const LikeMsg = {
+const LikeMsg = {
     times: ProtoField(1, ScalarType.INT32),
     time: ProtoField(2, ScalarType.INT32),
     detail: ProtoField(3, () => LikeDetail)
 };
 
-export const ProfileLikeSubTip = {
+const ProfileLikeSubTip = {
     msg: ProtoField(14, () => LikeMsg)
 };
 
-export const ProfileLikeTip = {
+const ProfileLikeTip = {
     msgType: ProtoField(1, ScalarType.INT32),
     subType: ProtoField(2, ScalarType.INT32),
     content: ProtoField(203, () => ProfileLikeSubTip)
 };
 
-export const SysMessageHeader = {
+const SysMessageHeader = {
     PeerNumber: ProtoField(1, ScalarType.UINT32),
     PeerString: ProtoField(2, ScalarType.STRING),
     Uin: ProtoField(5, ScalarType.UINT32),
     Uid: ProtoField(6, ScalarType.STRING, true)
 };
 
-export const SysMessageMsgSpec = {
+const SysMessageMsgSpec = {
     msgType: ProtoField(1, ScalarType.UINT32),
     subType: ProtoField(2, ScalarType.UINT32),
     subSubType: ProtoField(3, ScalarType.UINT32),
@@ -40,11 +40,11 @@ export const SysMessageMsgSpec = {
     other: ProtoField(13, ScalarType.UINT32)
 };
 
-export const SysMessageBodyWrapper = {
+const SysMessageBodyWrapper = {
     wrappedBody: ProtoField(2, ScalarType.BYTES)
 };
 
-export const SysMessage = {
+const SysMessage = {
     header: ProtoField(1, () => SysMessageHeader, false, true),
     msgSpec: ProtoField(2, () => SysMessageMsgSpec, false, true),
     bodyWrapper: ProtoField(3, () => SysMessageBodyWrapper)
