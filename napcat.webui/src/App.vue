f83bf197d2d11bf5e9b57284b4925d3ac61bc4d9
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-14 22:02:15 +0800

    feat: QQLogin

--- napcat.webui/src/App.vue
+++ napcat.webui/src/App.vue
@@ -1,9 +1,25 @@
-<template>
+<!-- <template>
   <div id="app">
     <LoginForm />
   </div>
 </template>
 
 <script setup>
-import LoginForm from './components/WebUiLogin.vue';
+import LoginForm from './components/QQ.vue';
+</script> -->
+<template>
+  <div id="app">
+    <QQLogin />
+  </div>
+</template>
+
+<script>
+import QQLogin from './components/QQLogin.vue';
+
+export default {
+  name: 'App',
+  components: {
+    QQLogin
+  }
+};
 </script>
--- napcat.webui/src/main.ts
+++ napcat.webui/src/main.ts
@@ -1,12 +1,22 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { Button as TButton, Input as TInput, Form as TForm, FormItem as TFormItem } from 'tdesign-vue-next';
+import {
+    Button as TButton,
+    Input as TInput,
+    Form as TForm,
+    FormItem as TFormItem,
+    Select as TSelect,
+    Option as TOption
+
+} from 'tdesign-vue-next';
 
 const app = createApp(App);
 app.use(TButton);
 app.use(TInput);
 app.use(TForm);
 app.use(TFormItem);
+app.use(TSelect);
+app.use(TOption);
 
 app.mount('#app');
