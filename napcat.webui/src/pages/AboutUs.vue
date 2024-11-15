53e0d70679b5690f697bb7009e97743a2a79fe07
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-15 13:25:02 +0800

    feat: 布局面板基础结构

---
+++ napcat.webui/src/pages/AboutUs.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="about-us">
+        <h1>面板关于信息</h1>
+        <p>这里显示面板的关于信息。</p>
+    </div>
+</template>
---
+++ napcat.webui/src/pages/OtherConfig.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="other-config">
+        <h1>面板其余配置</h1>
+        <p>这里显示面板的其余配置。</p>
+    </div>
+</template>
