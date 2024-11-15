e44595334a24d69c2dd5b27edfd81a997e4285d5
pk5ls20 <pk5ls20@outlook.com>
2024-11-15 20:39:18 +0800

    perf: avoid silk encoding blocking the main event loop (#527)

---
+++ src/common/audio-worker.ts
@@ -0,0 +1,9 @@
+import { encode } from "silk-wasm";
+
+export interface EncodeArgs {
+    input: ArrayBufferView | ArrayBuffer
+    sampleRate: number
+}
+export default async ({ input, sampleRate }: EncodeArgs) => {
+    return await encode(input, sampleRate);
+};
