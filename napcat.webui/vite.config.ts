fea65b73cdc6137d232c3383d7cce048c5b2c7d2
pk5ls20 <pk5ls20@outlook.com>
2024-11-16 00:13:05 +0800

    fix: vite config

--- napcat.webui/vite.config.ts
+++ napcat.webui/vite.config.ts
@@ -1,8 +1,14 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [vue()],
     base: './',
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, 'src'),
+        },
+    },
 });
