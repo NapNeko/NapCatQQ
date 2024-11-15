0815c2f07fa686d69d7a49e941cb87b8de222779
pk5ls20 <pk5ls20@outlook.com>
2024-11-16 06:10:36 +0800

    refactor: more comprehensive dev and prod env isolation and build process

--- napcat.webui/vite.config.ts
+++ napcat.webui/vite.config.ts
@@ -11,4 +11,9 @@ export default defineConfig({
             '@': path.resolve(__dirname, 'src'),
         },
     },
+    server: {
+        proxy: {
+            '/api': 'http://localhost:6099',
+        },
+    },
 });
