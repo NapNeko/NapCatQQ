12f47bdb27528ee49e7195b7fb50827f49b38774
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-16 11:14:21 +0800

    fix: 逻辑操作

--- napcat.webui/src/pages/OtherConfig.vue
+++ napcat.webui/src/pages/OtherConfig.vue
@@ -1,17 +1,21 @@
 <template>
-    <div class="other-config">
-        <div>
-            <t-divider content="其余配置" align="left" />
+    <div>
+        <t-divider content="其余配置" align="left" />
+    </div>
+    <div class="other-config-container">
+        <div class="other-config">
+            <t-form ref="form" :model="otherConfig" class="form">
+                <t-form-item label="音乐签名地址" name="musicSignUrl" class="form-item">
+                    <t-input v-model="otherConfig.musicSignUrl" />
+                </t-form-item>
+                <t-form-item label="启用本地文件到URL" name="enableLocalFile2Url" class="form-item">
+                    <t-switch v-model="otherConfig.enableLocalFile2Url" />
+                </t-form-item>
+            </t-form>
+            <div class="button-container">
+                <t-button @click="saveConfig">保存</t-button>
+            </div>
         </div>
-        <t-form ref="form" :model="otherConfig" class="form">
-            <t-form-item label="音乐签名地址" name="musicSignUrl" class="form-item">
-                <t-input v-model="otherConfig.musicSignUrl" />
-            </t-form-item>
-            <t-form-item label="启用本地文件到URL" name="enableLocalFile2Url" class="form-item">
-                <t-switch v-model="otherConfig.enableLocalFile2Url" />
-            </t-form-item>
-        </t-form>
-        <t-button @click="saveConfig">保存</t-button>
     </div>
 </template>
 
@@ -83,8 +87,20 @@ onMounted(() => {
 </script>
 
 <style scoped>
-.other-config {
+.other-config-container {
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
     padding: 20px;
+    box-sizing: border-box;
+}
+
+.other-config {
+    width: 100%;
+    max-width: 600px;
+    background: #fff;
+    padding: 20px;
+    border-radius: 8px;
 }
 
 .form {
@@ -96,6 +112,11 @@ onMounted(() => {
     display: flex;
     flex-direction: column;
     margin-bottom: 20px;
+}
+
+.button-container {
+    display: flex;
+    justify-content: center;
 }
 
 @media (min-width: 768px) {
