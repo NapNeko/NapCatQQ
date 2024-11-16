e80ed3b33e429d976fc790ff40ebd4180247825d
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-16 13:47:33 +0800

    fix: 切分依赖

--- napcat.webui/vite.config.ts
+++ napcat.webui/vite.config.ts
@@ -16,4 +16,15 @@ export default defineConfig({
             '/api': 'http://localhost:6099',
         },
     },
+    build: {
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        }
+    }
 });
