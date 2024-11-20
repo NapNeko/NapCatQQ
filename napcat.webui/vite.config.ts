ee8f381341e0b8445626acde867384d7ecf45c47
huankong233 <huank0ng@foxmail.com>
2024-11-20 14:02:34 +0800

    feat: 增强webui对老设备的支持

--- napcat.webui/vite.config.ts
+++ napcat.webui/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
+import legacy from '@vitejs/plugin-legacy';
 import path from 'path';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [vue()],
+    plugins: [
+        vue(),
+        legacy({
+            targets: ['defaults', 'not IE 11'],
+            modernPolyfills: ['web.structured-clone'],
+        }),
+    ],
     base: './',
     resolve: {
         alias: {
@@ -17,14 +24,18 @@ export default defineConfig({
         },
     },
     build: {
+        chunkSizeWarningLimit: 4000,
         rollupOptions: {
             output: {
-                manualChunks(id) {
+                chunkFileNames: 'static/js/[name]-[hash].js',
+                entryFileNames: 'static/js/[name]-[hash].js',
+                assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
+                manualChunks(id: string) {
                     if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                        return id.toString().split('node_modules/.pnpm/')[1].split('/')[0].toString();
                     }
-                }
-            }
-        }
-    }
+                },
+            },
+        },
+    },
 });
