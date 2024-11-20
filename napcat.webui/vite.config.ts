045ed24451a89e9898d3332d861e5711c7e8185f
huankong233 <huank0ng@foxmail.com>
2024-11-20 14:15:03 +0800

    revent: 对手动拆分chunk进行回滚

--- napcat.webui/vite.config.ts
+++ napcat.webui/vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
                 assetFileNames: 'static/[ext]/[name]-[hash].[ext]',
                 manualChunks(id: string) {
                     if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/.pnpm/')[1].split('/')[0].toString();
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
                     }
                 },
             },
