68036936a0be8cda2a1b72fd54f6315d75a7cc0c
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-22 15:58:40 +0800

    fix: GetProfileLike

--- src/onebot/action/extends/GetProfileLike.ts
+++ src/onebot/action/extends/GetProfileLike.ts
@@ -1,18 +1,31 @@
 import { OneBotAction } from '@/onebot/action/OneBotAction';
 import { ActionName } from '@/onebot/action/router';
+import { FromSchema, JSONSchema } from 'json-schema-to-ts';
 
-interface Payload {
-    start: number,
-    count: number
-}
+const SchemaData = {
+    type: 'object',
+    properties: {
+        user_id: { type: ['number', 'string'] },
+        start: { type: ['number', 'string'] },
+        count: { type: ['number', 'string'] },
+        type: { type: ['number', 'string'] },
+    },
+} as const satisfies JSONSchema;
+
+type Payload = FromSchema<typeof SchemaData>;
 
 export class GetProfileLike extends OneBotAction<Payload, any> {
     actionName = ActionName.GetProfileLike;
-
+    payloadSchema = SchemaData;
     async _handle(payload: Payload) {
         const start = payload.start ? Number(payload.start) : 0;
         const count = payload.count ? Number(payload.count) : 10;
-        const ret = await this.core.apis.UserApi.getProfileLike(this.core.selfInfo.uid, start, count);
+        const type = payload.count ? Number(payload.count) : 2;
+        const user_uid =
+            this.core.selfInfo.uin === payload.user_id || !payload.user_id ?
+                this.core.selfInfo.uid :
+                await this.core.apis.UserApi.getUidByUinV2(payload.user_id.toString());
+        const ret = await this.core.apis.UserApi.getProfileLike(user_uid ?? this.core.selfInfo.uid, start, count, type);
         const listdata = ret.info.userLikeInfos[0].voteInfo.userInfos;
         for (const item of listdata) {
             item.uin = parseInt((await this.core.apis.UserApi.getUinByUidV2(item.uid)) || '');
