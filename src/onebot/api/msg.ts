a1079dd948e5877fdcea162135e9f85abc2c1c49
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-25 21:58:35 +0800

    fix

--- src/onebot/api/msg.ts
+++ src/onebot/api/msg.ts
@@ -920,11 +920,11 @@ export class OneBotMsgApi {
                         fsPromise.unlink(file).then().catch(e => this.core.context.logger.logError.bind(this.core.context.logger)('发送消息删除文件失败', e));
                     }
                 } catch (error) {
-                    this.core.context.logger.logError.bind(this.core.context.logger)('发送消息删除文件失败', (e as Error).message)
+                    this.core.context.logger.logError.bind(this.core.context.logger)('发送消息删除文件失败', (error as Error).message)
                 }
             });
         }, 60000);
-        
+
         return returnMsg;
     }
 
