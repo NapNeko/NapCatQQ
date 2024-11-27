ed347f2484e9867095bb73200f5180752e9dd840
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-27 20:38:39 +0800

    fix: pic size

--- src/core/helper/msg.ts
+++ src/core/helper/msg.ts
@@ -3,12 +3,12 @@ import { PicType } from '../types';
 export async function getFileTypeForSendType(picPath: string): Promise<PicType> {
     const fileTypeResult = (await fileType.fileTypeFromFile(picPath))?.ext ?? 'jpg';
     const picTypeMap: { [key: string]: PicType } = {
-        'webp': PicType.NEWPIC_WEBP,
+        //'webp': PicType.NEWPIC_WEBP,
         'gif': PicType.NEWPIC_GIF,
-        'png': PicType.NEWPIC_APNG,
-        'jpg': PicType.NEWPIC_JPEG,
-        'jpeg': PicType.NEWPIC_JPEG,
-        'bmp': PicType.NEWPIC_BMP,
+        // 'png': PicType.NEWPIC_APNG,
+        // 'jpg': PicType.NEWPIC_JPEG,
+        // 'jpeg': PicType.NEWPIC_JPEG,
+        // 'bmp': PicType.NEWPIC_BMP,
     };
     return picTypeMap[fileTypeResult] ?? PicType.NEWPIC_JPEG;
 }
