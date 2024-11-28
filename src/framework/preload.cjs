97cacb43835bfb040ab86a1df6771c004f19f7d9
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-28 20:00:24 +0800

    refactor: framework的操作性

--- src/framework/preload.cjs
+++ src/framework/preload.cjs
@@ -1,10 +1,11 @@
-const { contextBridge } = require('electron');
-const { ipcRenderer } = require('electron');
-
+const { contextBridge, ipcRenderer } = require('electron');
 const napcat = {
     getWebUiUrl: async () => {
         return ipcRenderer.invoke('napcat_get_webtoken');
     },
+    openExternalUrl: async (url) => {
+        ipcRenderer.send('open_external_url', url);
+    }
 };
 // 在window对象下导出只读对象
 contextBridge.exposeInMainWorld('napcat', napcat);
