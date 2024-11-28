9f19ceca8732638b79bf7bf49f3a732424d4a8a0
手瓜一十雪 <nanaeonn@outlook.com>
2024-11-28 20:00:24 +0800

    refactor: framework的操作性

--- src/framework/liteloader.cjs
+++ src/framework/liteloader.cjs
@@ -1,6 +1,10 @@
 //LiteLoader需要提供部分IPC接口，以便于其他插件调用
 const { ipcMain } = require('electron');
 const napcat = require('./napcat.cjs');
+const { shell } = require('electron');
 ipcMain.handle('napcat_get_webtoken', async (event, arg) => {
     return napcat.NCgetWebUiUrl();
+});
+ipcMain.on('open_external_url', (event, url) => {
+    shell.openExternal(url);
 });
