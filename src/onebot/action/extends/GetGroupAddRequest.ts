da7636e60c376db1d962457bca4bb5670a302960
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-03 21:50:12 +0800

    fix: #592

--- src/onebot/action/extends/GetGroupAddRequest.ts
+++ src/onebot/action/extends/GetGroupAddRequest.ts
@@ -18,7 +18,7 @@ export default class GetGroupAddRequest extends OneBotAction<null, OB11GroupRequ
                 ignoredNotifies
                     .filter(notify => notify.type === 7)
                     .map(async SSNotify => ({
-                        request_id: SSNotify.seq,
+                        request_id: SSNotify.group.groupCode + '|' + SSNotify.seq + '|' + SSNotify.type,
                         requester_uin: await this.core.apis.UserApi.getUinByUidV2(SSNotify.user1?.uid),
                         requester_nick: SSNotify.user1?.nickName,
                         group_id: SSNotify.group?.groupCode,
