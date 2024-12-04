e939ec0e52473b2d4968ee0a75430fe0eb7c5810
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-04 20:37:08 +0800

    fix: #597

--- src/core/services/NodeIKernelGroupService.ts
+++ src/core/services/NodeIKernelGroupService.ts
@@ -187,11 +187,11 @@ export interface NodeIKernelGroupService {
 
     destroyGroup(groupCode: string): void;
 
-    getSingleScreenNotifies(doubted: boolean, start_seq: string, num: number): Promise<GeneralCallResult>;
+    getSingleScreenNotifies(doubt: boolean, startSeq: string, count: number): Promise<GeneralCallResult>;
 
     clearGroupNotifies(groupCode: string): void;
 
-    getGroupNotifiesUnreadCount(unknown: boolean): Promise<GeneralCallResult>;
+    getGroupNotifiesUnreadCount(doubt: boolean): Promise<GeneralCallResult>;
 
     clearGroupNotifiesUnreadCount(doubt: boolean): void;
 
