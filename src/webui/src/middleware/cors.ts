7b3c287137bbbb8cc048766d9bf888353d559900
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-05 19:37:56 +0800

    fix: cors

--- src/webui/src/middleware/cors.ts
+++ src/webui/src/middleware/cors.ts
@@ -3,7 +3,8 @@ import type { RequestHandler } from 'express';
 // CORS 中间件，跨域用
 export const cors: RequestHandler = (_, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.header('Access-Control-Allow-Methods', '*');
+    res.header('Access-Control-Allow-Headers', '*');
+    res.header('Access-Control-Allow-Credentials', 'true');
     next();
 };
