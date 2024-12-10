5461d48644c72af35464e53ec548b6edf57c2942
纸凤孤凰 <3100542635@qq.com>
2024-12-10 16:59:57 +0800

    fix: 路由守卫

--- napcat.webui/src/utils/request.ts
+++ napcat.webui/src/utils/request.ts
@@ -1,10 +1,10 @@
 import { MessagePlugin } from 'tdesign-vue-next';
-import { router } from '@/router/index.js';
+import router from '@/router/index.js';
 
 export const request = async (input: RequestInfo | URL, init?: RequestInit) => {
     const res = await fetch(input, init);
     const json = await res.json();
-    if (json.msg.includes('Unauthorized')) {
+    if (json.message.includes('Unauthorized')) {
         MessagePlugin.error('Token 过期啦, 重新登录吧');
         localStorage.clear();
         router.push('/webui');
