2c3b7e9ee86f5d6a22ff67eb66635f3fc7ba9d59
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-13 19:28:04 +0800

    fix: fuck! tencent

--- src/framework/liteloader.cjs
+++ src/framework/liteloader.cjs
@@ -1,5 +1,5 @@
 //LiteLoader需要提供部分IPC接口，以便于其他插件调用
-const { ipcMain } = require('electron');
+const { ipcMain, BrowserWindow } = require('electron');
 const napcat = require('./napcat.cjs');
 const { shell } = require('electron');
 ipcMain.handle('napcat_get_webtoken', async (event, arg) => {
@@ -13,4 +13,14 @@ ipcMain.handle('napcat_get_reactweb', async (event, arg) => {
     let port = url.port;
     let token = url.searchParams.get('token');
     return `https://napcat.152710.xyz/web_login?back=http://127.0.0.1:${port}&token=${token}`;
+});
+
+ipcMain.on('napcat_open_inner_url', (event, url) => {
+    const win = new BrowserWindow({
+        autoHideMenuBar: true,
+    });
+    win.loadURL(url);
+    win.webContents.setWindowOpenHandler(details => {
+        win.loadURL(details.url)
+    })
 });
