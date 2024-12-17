98dcfe11b9f3af303102ca90271810a4651ebd05
HanaKDev <62001729+HanaKDev@users.noreply.github.com>
2024-12-18 00:12:40 +0800

    稍微改进一下 KillQQ 避免误杀正常的QQ客户端
    牛魔的2025年都要来了怎么还有GBK呀喵
    喵爪印: 不要转换换行符和编码，避免出现不可控问题哦

---
+++ script/Kill_NapCat_UTF8.bat
@@ -0,0 +1,14 @@
+@echo off
+:: 别问怎么回事, 问就是我也不知道
+FOR /F "tokens=2 delims=," %%I IN ('tasklist /FI "WINDOWTITLE eq Administrator:  NapCat" /FO CSV /NH') DO (
+    SET "MainPID=%%~I"
+)
+TITLE NapCatPID^:%MainPID%
+if exist "%SystemRoot%\SysWOW64" path %path%;%windir%\SysNative;%SystemRoot%\SysWOW64;%~dp0
+bcdedit >nul
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else (goto UACAdmin)
+:UACPrompt
+%1 start "" mshta vbscript:createobject("shell.application").shellexecute("""%~0""","::",,"runas",1)(window.close)&exit
+exit /B
+:UACAdmin
+%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe -ExecutionPolicy ByPass -NoExit -Command "& '%~dp0Kill_NapCat.ps1'"
