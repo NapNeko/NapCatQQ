817e1304320c7648d00c10d7ba9a40529c8241fd
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-26 20:36:53 +0800

    feat: send_packet

---
+++ src/onebot/action/extends/SendPacket.ts
@@ -0,0 +1,21 @@
+import { GetPacketStatusDepends } from '@/onebot/action/packet/GetPacketStatus';
+import { ActionName } from '@/onebot/action/router';
+import { Static, Type } from '@sinclair/typebox';
+
+const SchemaData = Type.Object({
+    cmd: Type.String(),
+    data: Type.String(),
+    rsp: Type.Union([Type.String(), Type.Boolean()], { default: true }),
+});
+
+type Payload = Static<typeof SchemaData>;
+
+export class SendPacket extends GetPacketStatusDepends<Payload, any> {
+    payloadSchema = SchemaData;
+    actionName = ActionName.SendPacket;
+    async _handle(payload: Payload) {
+        const rsp = typeof payload.rsp === 'boolean' ? payload.rsp : payload.rsp === 'true';
+        const data = await this.core.apis.PacketApi.pkt.client.sendOidbPacket({ cmd: payload.cmd, data: payload.data as any }, rsp);
+        return typeof data === 'object' ? data.toString('hex') : undefined;
+    }
+}
