2e8327f28c7fd7231ac2b5d1e3a78880913e5cc9
手瓜一十雪 <nanaeonn@outlook.com>
2024-12-30 13:23:33 +0800

    fix

--- src/common/umami.ts
+++ src/common/umami.ts
@@ -19,7 +19,7 @@ export class umamiTrace {
                 'website': '596cbbb2-1740-4373-a807-cf3d0637bfa7',
                 'hostname': 'trace.napneko.icu',
                 'title': 'NapCat ' + umamiTrace.napcatVersion,
-                'url': '/' + umamiTrace.qqversion + '/' + umamiTrace.napcatVersion + '/' + eventName + (info ? '' : '/' + info),
+                'url': '/' + umamiTrace.qqversion + '/' + umamiTrace.napcatVersion + '/' + eventName + (!info ? '' : '/' + info),
                 'referrer': 'https://napcat.onebot.napneko.icu/',
             }
         };
