3ef541dd7850612b39c59405bd1e7acee41d7f0c
bietiaop <1527109126@qq.com>
2025-02-02 14:47:34 +0800

    fix: 频率限制

--- src/webui/src/router/File.ts
+++ src/webui/src/router/File.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import rateLimit from 'express-rate-limit';
 import {
     ListFilesHandler,
     CreateDirHandler,
@@ -14,13 +15,20 @@ import {
 
 const router = Router();
 
+const apiLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1分钟内
+    max: 60, // 最大60个请求
+});
+
+router.use(apiLimiter);
+
 router.get('/list', ListFilesHandler);
 router.post('/mkdir', CreateDirHandler);
 router.post('/delete', DeleteHandler);
 router.get('/read', ReadFileHandler);
 router.post('/write', WriteFileHandler);
 router.post('/create', CreateFileHandler);
-router.post('/batchDelete', BatchDeleteHandler); // 添加这一行
+router.post('/batchDelete', BatchDeleteHandler);
 router.post('/rename', RenameHandler);
 router.post('/move', MoveHandler);
 router.post('/batchMove', BatchMoveHandler);
