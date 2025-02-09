45c5965b99b1934bd43328d7bb30cff1d4af1024
bietiaop <1527109126@qq.com>
2025-02-09 12:11:27 +0800

    style: 增加heroui主题色

--- napcat.webui/src/const/themes.ts
+++ napcat.webui/src/const/themes.ts
@@ -1,5 +1,6 @@
+import heroui from './themes/heroui'
 import nc_pink from './themes/nc_pink'
 
-const themes: ThemeInfo[] = [nc_pink]
+const themes: ThemeInfo[] = [nc_pink, heroui]
 
 export default themes
