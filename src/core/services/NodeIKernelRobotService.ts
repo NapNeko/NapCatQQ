a2882d17e18b6e73d4721a677932d09010e4f79c
手瓜一十雪 <nanaeonn@outlook.com>
2025-02-14 15:39:06 +0800

    feat: 补全一些type

--- src/core/services/NodeIKernelRobotService.ts
+++ src/core/services/NodeIKernelRobotService.ts
@@ -1,4 +1,5 @@
 import { NodeIKernelRobotListener } from '@/core/listeners';
+import { GeneralCallResult, Peer } from '..';
 
 export interface NodeIKernelRobotService {
     fetchGroupRobotStoreDiscovery(arg: unknown): unknown;
@@ -30,6 +31,18 @@ export interface NodeIKernelRobotService {
     setRobotPickTts(arg1: unknown, arg2: unknown): unknown;
 
     getRobotUinRange(data: unknown): Promise<{ response: { robotUinRanges: Array<unknown> } }>;
+
+    getRobotFunctions(peer: Peer, params: {
+        uins: Array<string>,
+        num: 0,
+        client_info: { platform: 4, version: '', build_num: 9999 },
+        tinyids: [],
+        page: 0,
+        full_fetch: false,
+        scene: 4,
+        filter: 1,
+        bkn: ''
+    }): Promise<GeneralCallResult & { response: { bot_features: Array<unknown>, next_page: number } }>;
 
     isNull(): boolean;
 }
