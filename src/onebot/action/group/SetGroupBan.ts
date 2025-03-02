165435e3b32f2852f7dbbb2e5ee1468c260a1c15
手瓜一十雪 <nanaeonn@outlook.com>
2025-03-02 14:55:47 +0800

    fix: #785

--- src/onebot/action/group/SetGroupBan.ts
+++ src/onebot/action/group/SetGroupBan.ts
@@ -16,6 +16,9 @@ export default class SetGroupBan extends OneBotAction<Payload, null> {
     async _handle(payload: Payload): Promise<null> {
         const uid = await this.core.apis.UserApi.getUidByUinV2(payload.user_id.toString());
         if (!uid) throw new Error('uid error');
+        let member_role = (await this.core.apis.GroupApi.getGroupMemberEx(payload.group_id.toString(), uid, true))?.role;
+        if (member_role === 4) throw new Error('cannot ban owner');
+        if (member_role === 3) throw new Error('cannot ban admin');
         // 例如无管理员权限时 result为 120101005 errMsg为 'ERR_NOT_GROUP_ADMIN'
         let ret = await this.core.apis.GroupApi.banMember(payload.group_id.toString(),
             [{ uid: uid, timeStamp: +payload.duration }]);
