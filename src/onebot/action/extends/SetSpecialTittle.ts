2e7176304ba94ef80a29d3308391543feadc958e
手瓜一十雪 <nanaeonn@outlook.com>
2025-03-02 14:24:51 +0800

    fix: #843

--- src/onebot/action/extends/SetSpecialTittle.ts
+++ src/onebot/action/extends/SetSpecialTittle.ts
@@ -5,7 +5,7 @@ import { Static, Type } from '@sinclair/typebox';
 const SchemaData = Type.Object({
     group_id: Type.Union([Type.Number(), Type.String()]),
     user_id: Type.Union([Type.Number(), Type.String()]),
-    special_title: Type.String(),
+    special_title: Type.String({ default: '' }),
 });
 
 type Payload = Static<typeof SchemaData>;
@@ -16,7 +16,7 @@ export class SetSpecialTittle extends GetPacketStatusDepends<Payload, void> {
 
     async _handle(payload: Payload) {
         const uid = await this.core.apis.UserApi.getUidByUinV2(payload.user_id.toString());
-        if(!uid) throw new Error('User not found');
+        if (!uid) throw new Error('User not found');
         await this.core.apis.PacketApi.pkt.operation.SetGroupSpecialTitle(+payload.group_id, uid, payload.special_title);
     }
 }
