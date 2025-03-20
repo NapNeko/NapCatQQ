79aa1dc67fec26bab15bf96dd50e57e8565d7cfd
手瓜一十雪 <nanaeonn@outlook.com>
2025-03-20 10:32:13 +0800

    fix: #883

--- src/onebot/action/extends/GetProfileLike.ts
+++ src/onebot/action/extends/GetProfileLike.ts
@@ -6,7 +6,8 @@ import { Type, Static } from '@sinclair/typebox';
 const SchemaData = Type.Object({
     user_id: Type.Optional(Type.Union([Type.Number(), Type.String()])),
     start: Type.Union([Type.Number(), Type.String()], { default: 0 }),
-    count: Type.Union([Type.Number(), Type.String()], { default: 10 })
+    count: Type.Union([Type.Number(), Type.String()], { default: 10 }),
+    type: Type.Union([Type.Number(), Type.String()], { default: 2 })
 });
 
 type Payload = Static<typeof SchemaData>;
@@ -33,8 +34,7 @@ export class GetProfileLike extends OneBotAction<Payload, {
     async _handle(payload: Payload) {
         const isSelf = this.core.selfInfo.uin === payload.user_id || !payload.user_id;
         const userUid = isSelf || !payload.user_id ? this.core.selfInfo.uid : await this.core.apis.UserApi.getUidByUinV2(payload.user_id.toString());
-        const type = isSelf ? 2 : 1;
-        const ret = await this.core.apis.UserApi.getProfileLike(userUid ?? this.core.selfInfo.uid, +payload.start, +payload.count, type);
+        const ret = await this.core.apis.UserApi.getProfileLike(userUid ?? this.core.selfInfo.uid, +payload.start, +payload.count, +payload.type);
         const data = ret.info.userLikeInfos[0];
         if (!data) {
             throw new Error('get info error');
