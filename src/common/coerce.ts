ef59181dcde4061361abbeb1c58dda9213ccb7ff
手瓜一十雪 <nanaeonn@outlook.com>
2025-04-19 10:53:29 +0800

    Revert "fix: coerce"
    This reverts commit 0f508a511cb0bc095a526832cd04810a22ebc706.

--- src/common/coerce.ts
+++ src/common/coerce.ts
@@ -1,26 +1,14 @@
 import { z } from 'zod';
-
 const boolean = () => z.preprocess(
-    val => val === null || val === undefined
-        ? val
-        : typeof val === 'string' && (val.toLowerCase() === 'false' || val === '0')
-            ? false
-            : Boolean(val),
+    val => typeof val === 'string' && (val.toLowerCase() === 'false' || val === '0') ? false : Boolean(val),
     z.boolean()
 );
-
 const number = () => z.preprocess(
-    val => val === null || val === undefined
-        ? val
-        : typeof val !== 'number' ? Number(val) : val,
+    val => typeof val !== 'number' ? Number(val) : val,
     z.number()
 );
-
 const string = () => z.preprocess(
-    val => val === null || val === undefined
-        ? val
-        : typeof val !== 'string' ? String(val) : val,
+    val => typeof val !== 'string' ? String(val) : val,
     z.string()
 );
-
 export const coerce = { boolean, number, string };
--- src/onebot/action/group/SendGroupMsg.ts
+++ src/onebot/action/group/SendGroupMsg.ts
@@ -1,15 +1,16 @@
 import { ContextMode, SendMsgBase } from '@/onebot/action/msg/SendMsg';
-import { ActionName } from '@/onebot/action/router';
+import { ActionName, BaseCheckResult } from '@/onebot/action/router';
 import { OB11PostSendMsg } from '@/onebot/types';
 
 // 未检测参数
 class SendGroupMsg extends SendMsgBase {
     override actionName = ActionName.SendGroupMsg;
     override contextMode: ContextMode = ContextMode.Group;
-    override async _handle(payload: OB11PostSendMsg) {
+
+    protected override async check(payload: OB11PostSendMsg): Promise<BaseCheckResult> {
         delete payload.user_id;
         payload.message_type = 'group';
-        return super._handle(payload);
+        return super.check(payload);
     }
 }
 
