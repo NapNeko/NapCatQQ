eb8ec6670e85bb90d2e114e17feb151dd6d6db5d
Nepenthe <Ttjonne@outlook.com>
2025-05-18 18:32:23 +0800

    增加抽象类，修改继承关系

--- src/onebot/event/request/OB11GroupRequest.ts
+++ src/onebot/event/request/OB11GroupRequest.ts
@@ -1,18 +1,18 @@
-import { OB11GroupNoticeEvent } from '@/onebot/event/notice/OB11GroupNoticeEvent';
-import { EventType } from '@/onebot/event/OneBotEvent';
 import { NapCatCore } from '@/core';
+import { OB11BaseRequestEvent } from './OB11BaseRequestEvent';
 
-export class OB11GroupRequestEvent extends OB11GroupNoticeEvent {
-    override post_type = EventType.REQUEST;
+export class OB11GroupRequestEvent extends OB11BaseRequestEvent {
     request_type = 'group';
 
-    override user_id: number;
+    group_id: number;
+    user_id: number;
     comment: string;
     flag: string;
     sub_type: string;
 
     constructor(core: NapCatCore, groupId: number, userId: number, sub_type: string, comment: string, flag: string) {
-        super(core, groupId, userId);
+        super(core);
+        this.group_id = groupId;
         this.user_id = userId;
         this.sub_type = sub_type;
         this.comment = comment;
