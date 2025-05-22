5dd3bade534ec0089e1cc016dfcff2a477894913
手瓜一十雪 <nanaeonn@outlook.com>
2025-05-22 11:47:31 +0800

    fix: #1029

--- src/webui/src/terminal/init-dynamic-dirname.ts
+++ src/webui/src/terminal/init-dynamic-dirname.ts
@@ -9,7 +9,7 @@ Object.defineProperty(global, '__dirname', {
         // 注意：堆栈格式可能不同，请根据实际环境调整索引及正则表达式
         for (const line of stack) {
             const match = line.match(/\((.*):\d+:\d+\)/);
-            if (match) {
+            if (match?.[1]) {
                 callerFile = match[1];
                 if (!callerFile.includes('init-dynamic-dirname.ts')) {
                     break;
