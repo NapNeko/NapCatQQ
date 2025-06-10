330393f068fe24bc823beabcc9cde713a95cf4aa
时瑾 <74231782+sj817@users.noreply.github.com>
2025-06-11 00:25:30 +0800

    fix: `create_group_file_folder` 接口兼容性提升

--- src/onebot/action/go-cqhttp/CreateGroupFileFolder.ts
+++ src/onebot/action/go-cqhttp/CreateGroupFileFolder.ts
@@ -4,7 +4,10 @@ import { Static, Type } from '@sinclair/typebox';
 
 const SchemaData = Type.Object({
     group_id: Type.Union([Type.Number(), Type.String()]),
-    folder_name: Type.String(),
+    // 兼容gocq 与name二选一
+    folder_name: Type.Optional(Type.String()),
+    // 兼容gocq 与folder_name二选一
+    name: Type.Optional(Type.String()),
 });
 
 type Payload = Static<typeof SchemaData>;
@@ -16,6 +19,7 @@ export class CreateGroupFileFolder extends  OneBotAction<Payload, ResponseType> 
     override actionName = ActionName.GoCQHTTP_CreateGroupFileFolder;
     override payloadSchema = SchemaData;
     async _handle(payload: Payload) {
-        return (await this.core.apis.GroupApi.creatGroupFileFolder(payload.group_id.toString(), payload.folder_name)).resultWithGroupItem;
+        const folderName = payload.folder_name || payload.name;
+        return (await this.core.apis.GroupApi.creatGroupFileFolder(payload.group_id.toString(), folderName!)).resultWithGroupItem;
     }
 }
