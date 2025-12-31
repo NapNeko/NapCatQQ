527de5a3679611f911c5025f228a1c71a164e559
Nepenthe <113700307+pohgxz@users.noreply.github.com>
2025-12-31 13:58:55 +0800

    更新插件示例，修复插件打包问题 (#1486)
    * fix: 修复打包错误
    
    * fix: 完善插件模板
    
    * Update index.ts

--- packages/napcat-plugin/index.ts
+++ packages/napcat-plugin/index.ts
@@ -1,12 +1,22 @@
+import { createActionMap } from 'napcat-onebot/action';
 import { EventType } from 'napcat-onebot/event/OneBotEvent';
 import type { PluginModule } from 'napcat-onebot/network/plugin';
 
+/**
+ * 导入 napcat 包时候不使用 @/napcat...，直接使用 napcat...
+ * 因为 @/napcat... 会导致打包时包含整个 napcat 包，而不是只包含需要的部分
+ */
+
+// action 作为参数传递时请用这个
+let actionMap: ReturnType<typeof createActionMap> | undefined = undefined;
+
 const plugin_init: PluginModule['plugin_init'] = async (_core, _obContext, _actions, _instance) => {
   console.log('[Plugin: example] 插件已初始化');
+  actionMap = _actions;
 };
 const plugin_onmessage: PluginModule['plugin_onmessage'] = async (adapter, _core, _obCtx, event, actions, instance) => {
   if (event.post_type === EventType.MESSAGE && event.raw_message.includes('ping')) {
     await actions.get('send_group_msg')?.handle({ group_id: String(event.group_id), message: 'pong' }, adapter, instance.config);
   }
 };
-export { plugin_init, plugin_onmessage };
+export { plugin_init, plugin_onmessage, actionMap };
