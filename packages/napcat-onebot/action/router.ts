2caf6a56ad4d95cfca02130701e2bc009d8fe445
Hans155922 <35620329+Hans155922@users.noreply.github.com>
2026-01-25 10:52:47 +0800

    Update router.ts

--- packages/napcat-onebot/action/router.ts
+++ packages/napcat-onebot/action/router.ts
@@ -152,6 +152,7 @@ export const ActionName = {
   GetProfileLike: 'get_profile_like',
   FetchCustomFace: 'fetch_custom_face',
   FetchEmojiLike: 'fetch_emoji_like',
+  FetchEmojiLikesAll: 'fetch_emoji_likes_all',
   SetInputStatus: 'set_input_status',
   GetGroupInfoEx: 'get_group_info_ex',
   GetGroupDetailInfo: 'get_group_detail_info',
